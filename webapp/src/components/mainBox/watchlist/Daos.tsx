c96be8affcaac22d747a817a2b388caca5f94493
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-09-30 11:38:56 +0300

    migrated everything to t3app

---
+++ webapp/src/components/mainBox/watchlist/Daos.tsx
@@ -0,0 +1,65 @@
+import {
+  Text,
+  VStack,
+  Divider,
+  Flex,
+  Spinner,
+  Center,
+  SimpleGrid,
+  Box,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { DaoType } from "./../../../types";
+import { SubscriptionItem } from "./DaoItem";
+
+const Subscriptions = () => {
+  const [subscribed, setSubscribed] = useState<DaoType[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`/api/daos`, { method: "GET" })
+      .then((response) => response.json())
+      .then(async (data) => {
+        setSubscribed(data);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <Box w="full">
+      <VStack
+        m={{ base: "0", md: "10" }}
+        align="start"
+        p={{ base: "2", md: "5" }}
+      >
+        <Text fontSize="3xl" fontWeight="800">
+          Watchlist
+        </Text>
+        <Box pb="0.3rem" pt="1rem" />
+        <Divider />
+        <Box pb="0.3rem" pt="1rem" />
+        {loading && (
+          <Center w="full">
+            <Spinner />
+          </Center>
+        )}
+        <SimpleGrid
+          minChildWidth="10rem"
+          spacing="1rem"
+          w="full"
+          justifyItems="center"
+        >
+          {subscribed.map((dao: DaoType, index: number) => {
+            return (
+              <Flex key={index}>
+                <SubscriptionItem dao={dao} />
+              </Flex>
+            );
+          })}
+        </SimpleGrid>
+      </VStack>
+    </Box>
+  );
+};
+
+export default Subscriptions;
---
+++ webapp/src/types/index.ts
@@ -0,0 +1,75 @@
+import { Prisma } from "@prisma/client";
+
+export type DaoType = Prisma.DaoGetPayload<null>;
+
+export enum DaoOnChainHandler {
+  None = 0,
+  Bravo1,
+  Bravo2,
+  Maker,
+}
+
+export type ProposalType = Prisma.ProposalGetPayload<{
+  include: {
+    dao: true;
+    userVote: {
+      include: {
+        user: true;
+      };
+    };
+  };
+}>;
+
+export enum ProposalTypeEnum {
+  Snapshot = 0,
+  Chain,
+}
+
+export type NotificationSettingType = Prisma.NotificationSettingGetPayload<{
+  select: {
+    createdTime: true;
+    delay: true;
+  };
+}>;
+
+export type NotificationChannelType = Prisma.NotificationChannelGetPayload<{
+  select: {
+    type: true;
+    connector: true;
+  };
+}>;
+
+export enum NotificationChannelEnum {
+  None = 0,
+  Discord,
+  Slack,
+  Telegram,
+  EPNS,
+}
+
+export enum NotificationIntervalEnum {
+  None = 0,
+  NewProposal = -1,
+  OneHour = 3600,
+  TwoHours = 3600 * 2,
+  ThreeHours = 3600 * 3,
+  SixHours = 3600 * 6,
+  TwelveHours = 3600 * 12,
+  OneDay = 86400,
+  TwoDays = 86400 * 2,
+  ThreeDays = 86400 * 3,
+}
+
+export enum PagesEnum {
+  None = 1,
+  Dashboard,
+  Watchlist,
+  Tracker,
+  Settings,
+}
+
+export interface LinkItemSPAProps {
+  name: string;
+  id: PagesEnum;
+  icon: number;
+}
