b445ebdea59a29b1fef028e72acac8f75efbaeae
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-09-30 19:16:22 +0300

    more turborepo

--- packages/database/src/seed.ts
+++ packages/database/src/seed.ts
@@ -539,12 +539,4 @@ async function main() {
   });
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+main();
