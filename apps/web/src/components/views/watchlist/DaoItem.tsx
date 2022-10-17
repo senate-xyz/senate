225f623f529b150c86451ffd6983d86aee99cd1b
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-10-17 15:53:12 +0300

    chromatic

--- apps/web/src/components/views/watchlist/DaoItem.tsx
+++ apps/web/src/components/views/watchlist/DaoItem.tsx
@@ -80,8 +80,8 @@ export const DaoItem = (props: {
         type="button"
         className={`${
           subscribed
-            ? "bg-emerald-500 text-white active:bg-emerald-600"
-            : "bg-pink-500 text-white active:bg-pink-600"
+            ? "bg-emerald-500 text-white active:bg-emerald-500"
+            : "bg-pink-400 text-white active:bg-pink-500"
         } font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
       >
         <div className="mt-4">
