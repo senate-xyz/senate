7ffa355773533285eeef91786823e055defe2cc4
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-10-18 14:21:05 +0300

    proxy addresses add

--- apps/web/src/components/views/settings/proxy-accounts/ProxyAccounts.tsx
+++ apps/web/src/components/views/settings/proxy-accounts/ProxyAccounts.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
 import { trpc } from '../../../../utils/trpc'
 
 const ProxyAccounts = () => {
+    const [newProxyAddress, setNewProxyAddress] = useState('')
     const proxyAddresses = trpc.useQuery(['user.proxyAddresses'])
+    const addProxy = trpc.useMutation('user.addProxy')
+    const removeProxy = trpc.useMutation('user.removeProxy')
+    const utils = trpc.useContext()
 
     if (!proxyAddresses.data) return <div>Loading</div>
 
@@ -10,8 +15,62 @@ const ProxyAccounts = () => {
             <p>ProxyAccounts</p>
 
             {proxyAddresses.data.map((proxyAddress) => {
-                return <div>{proxyAddress.address}</div>
+                return (
+                    <div className="flex flex-row items-center">
+                        {proxyAddress.address}
+
+                        <button
+                            onClick={() => {
+                                removeProxy.mutate(
+                                    { address: proxyAddress.address },
+                                    {
+                                        onSuccess() {
+                                            utils.invalidateQueries()
+                                        },
+                                    }
+                                )
+                            }}
+                            className="rounded-lg bg-blue-300 p-2 text-center text-sm font-medium"
+                        >
+                            Delete
+                        </button>
+                    </div>
+                )
             })}
+
+            <div className="border border-gray-300 p-3">
+                <label
+                    htmlFor="proxyAddress"
+                    className="m-2 block text-sm font-medium"
+                >
+                    Proxy address
+                </label>
+                <input
+                    type="text"
+                    id="proxyAddress"
+                    className="m-2 block w-full rounded-lg border p-2.5 text-sm"
+                    placeholder="0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+                    value={newProxyAddress}
+                    onChange={(e) => setNewProxyAddress(e.target.value)}
+                    required
+                />
+                <button
+                    onClick={() => {
+                        addProxy.mutate(
+                            { address: newProxyAddress },
+                            {
+                                onSuccess() {
+                                    utils.invalidateQueries()
+                                },
+                            }
+                        )
+                    }}
+                    type="submit"
+                    className="w-full rounded-lg bg-blue-300 px-5 py-2.5 text-center text-sm font-medium"
+                >
+                    Submit
+                </button>
+            </div>
         </div>
     )
 }
