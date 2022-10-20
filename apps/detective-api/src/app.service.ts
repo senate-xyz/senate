62b2d1bc3c1a944e29972b1cd431f961d745c05f
Eugen <eugen.patru@stream-financial.com>
2022-10-21 02:13:51 +0300

    WIP: Refactoring done. Time for testing

--- apps/detective-api/src/app.service.ts
+++ apps/detective-api/src/app.service.ts
@@ -1,8 +1,108 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+
+import { updateGovernorBravoProposals } from "./proposals/governorBravoProposals";
+import { updateSnapshotProposals } from "./proposals/snapshotProposals";
+import { updateMakerProposals } from "./proposals/makerProposals";
+import { updateMakerPolls } from "./proposals/makerPolls";
+import { updateSnapshotVotes } from "./votes/snapshotVotes";
+import { updateGovernorBravoVotes } from "./votes/governorBravoVotes";
+import { updateMakerVotes } from "./votes/makerVotes";
+import { updateMakerPollsVotes } from "./votes/makerPollsVotes";
+
+import { DAOType as Dao, DAOHandlerType as DAOHandler, User } from "@senate/common-types";
+import { prisma } from "@senate/database";
 
 @Injectable()
 export class AppService {
+
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
+    return 'Hello World!';
+  }
+
+  async updateProposals(daoId : string): Promise<string> {
+
+    this.logger.log(daoId);
+    let dao : Dao = await prisma.dAO.findFirst({
+      where: {
+        id: daoId
+      },
+      include: {
+        handlers: true,
+        subscriptions: true,
+      }
+    })
+
+    dao.handlers.forEach(async handler => {
+      switch(handler.type) {
+        case "SNAPSHOT": 
+          await updateSnapshotProposals(dao.name, handler);
+          break;
+        
+        case "BRAVO1" || "BRAVO2":
+          await updateGovernorBravoProposals(handler);
+          break;
+  
+        case "MAKER_EXECUTIVE":
+          await updateMakerProposals(handler);
+          break;
+  
+        case "MAKER_POLL":
+          await updateMakerPolls(handler);
+          break;
+  
+        default:
+          break;    
+      }
+    })
+
+    
+    return 'Hello!';
+  }
+
+  async updateVotes(daoId : string, userId: string): Promise<string> {
+    
+    let dao : Dao = await prisma.dAO.findFirst({
+      where: {
+        id: daoId
+      },
+      include: {
+        handlers: true,
+        subscriptions: true,
+      }
+    })
+
+    let user : User = await prisma.user.findFirst({
+      where: {
+        id: userId
+      }
+    })
+
+    dao.handlers.forEach(async handler => {
+      switch(handler.type) {
+        case "SNAPSHOT": 
+          await updateSnapshotVotes(handler, user, dao.name);
+          break;
+        
+        case "BRAVO1" || "BRAVO2":
+          await updateGovernorBravoVotes(handler, user, dao.name);
+          break;
+
+        case "MAKER_EXECUTIVE":
+          await updateMakerVotes(handler, user, dao.name);
+          break;
+
+        case "MAKER_POLL":
+          await updateMakerPollsVotes(handler, user, dao.name);
+          break;
+
+        default:
+          break;
+        
+      }
+    })
+
     return 'Hello World!';
   }
 }
---
+++ apps/detective-api/src/proposals/makerProposals.ts
@@ -0,0 +1,129 @@
+import { ethers } from "ethers";
+import axios from "axios";
+
+import { prisma } from "@senate/database";
+import { DAOHandler } from "@senate/common-types";
+
+const provider = new ethers.providers.JsonRpcProvider({
+  url: String(process.env.PROVIDER_URL),
+});
+
+export const updateMakerProposals = async (daoHandler: DAOHandler) => {
+  
+  const iface = new ethers.utils.Interface(daoHandler.decoder['abi']);
+  const chiefContract = new ethers.Contract(daoHandler.decoder['address'], daoHandler.decoder['abi'], provider);
+
+  const voteMultipleActionsTopic =
+    "0xed08132900000000000000000000000000000000000000000000000000000000";
+  const voteSingleActionTopic =
+    "0xa69beaba00000000000000000000000000000000000000000000000000000000";
+
+  const logs = await provider.getLogs({
+    fromBlock: daoHandler.decoder['latestBlock'],
+    address: daoHandler.decoder['address'],
+    topics: [[voteMultipleActionsTopic, voteSingleActionTopic]],
+  });
+
+  const spellAddressesSet = new Set<string>();
+  for (let i = 0; i < logs.length; i++) {
+    console.log(`maker event ${i} out of ${logs.length}`);
+
+    let log = logs[i];
+    let eventArgs = iface.decodeEventLog("LogNote", log.data);
+
+    let decodedFunctionData =
+      log.topics[0] === voteSingleActionTopic
+        ? iface.decodeFunctionData("vote(bytes32)", eventArgs.fax)
+        : iface.decodeFunctionData("vote(address[])", eventArgs.fax);
+
+    let spells: string[] =
+      decodedFunctionData.yays !== undefined
+        ? decodedFunctionData.yays
+        : await getSlateYays(chiefContract, decodedFunctionData.slate);
+
+    spells.forEach((spell) => {
+      spellAddressesSet.add(spell);
+    });
+  }
+
+  let spellAddresses = Array.from(spellAddressesSet);
+
+  for (let i = 0; i < spellAddresses.length; i++) {
+    console.log(`inserted ${spellAddresses[i]} spell address`);
+    try {
+      const response = await axios.get(
+        "https://vote.makerdao.com/api/executive/" + spellAddresses[i]
+      );
+
+      await prisma.proposal.upsert({
+        where: {
+            externalId_daoId: {
+                daoId: daoHandler.daoId,
+                externalId: spellAddresses[i],
+            },
+        },
+        update: {},
+        create: {
+          externalId: spellAddresses[i],
+          name: response.data.title,
+          description: response.data.content,
+          daoId: daoHandler.daoId,
+          daoHandlerId: daoHandler.id,
+          proposalType: daoHandler.type,
+          data: {
+            timeEnd: calculateVotingPeriodEndDate(response.data.spellData),
+            timeStart: response.data.date,
+            timeCreated: response.data.date,
+          },
+          url: daoHandler.decoder['proposalUrl'] + response.data.key,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  let latestBlock = await provider.getBlockNumber();
+  let decoder = daoHandler.decoder;
+    decoder['latestBlock'] = latestBlock + 1;
+
+  await prisma.dAOHandler.update({
+    where: {
+      id: daoHandler.id,
+    },
+    data: {
+      decoder: decoder,
+    },
+  });
+
+  console.log("\n\n");
+  console.log(`inserted ${spellAddresses.length} chain proposals`);
+  console.log("======================================================\n\n");
+  
+};
+
+const calculateVotingPeriodEndDate = (spellData: any) => {
+  return spellData.hasBeenCast
+    ? spellData.dateExecuted
+    : spellData.hasBeenScheduled
+    ? spellData.nextCastTime
+    : spellData.expiration;
+};
+
+const getSlateYays = async (chiefContract: ethers.Contract, slate: string) => {
+  let yays = [];
+  let count = 0;
+
+  while (true) {
+    let spellAddress;
+    try {
+      spellAddress = await chiefContract.slates(slate, count);
+      yays.push(spellAddress);
+      count++;
+    } catch (err) {
+      break;
+    }
+  }
+
+  return yays;
+};
