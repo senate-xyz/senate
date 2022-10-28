053c91e8ad808c6105c4e0ec042e6f1de1035198
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-10-28 21:24:33 +0300

    Update main.ts

--- apps/detective-api/src/main.ts
+++ apps/detective-api/src/main.ts
@@ -2,7 +2,7 @@ import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-const PORT = 3100
+const PORT = process.env.PORT || 3100
 
 async function bootstrap() {
     const logger = new Logger('Bootstrap')
