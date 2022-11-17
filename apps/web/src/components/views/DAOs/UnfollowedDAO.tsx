2cd12f6793237a0bdddadd3e055332a5ed567c60
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-11-17 16:11:51 +0200

    dao page kinda ready

--- apps/web/src/components/views/DAOs/UnfollowedDAO.tsx
+++ apps/web/src/components/views/DAOs/UnfollowedDAO.tsx
@@ -1,154 +1,25 @@
 import Image from 'next/image'
-import { useState } from 'react'
-import { FaBell, FaDiscord, FaSlack, FaTelegram } from 'react-icons/fa'
 
 import { trpc } from '../../../utils/trpc'
 import { inferProcedureOutput } from '@trpc/server'
 import { AppRouter } from '../../../server/trpc/router/_app'
 
 export const UnfollowedDAO = (props: {
-    dao:
-        | inferProcedureOutput<AppRouter['user']['userDaos']>[0]
-        | inferProcedureOutput<AppRouter['public']['daos']>[0]
+    dao: inferProcedureOutput<AppRouter['public']['daos']>[0]
+    refreshDaos: () => void
 }) => {
-    const [showModal, setShowModal] = useState(false)
-
     const subscribe = trpc.user.userSubscribe.useMutation()
-    const unsubscribe = trpc.user.userUnsubscribe.useMutation()
 
-    const refreshDao = trpc.public.refreshDao.useMutation({
-        onSuccess: () => {
-            refreshStatus.refetch()
-        },
-    })
     const refreshStatus = trpc.public.refreshStatus.useQuery({
         daoId: props.dao.id,
     })
-
-    const [subscribed, setSubscribed] = useState(
-        props.dao.subscriptions.length > 0 ? true : false
-    )
 
     if (!refreshStatus.data) return <div>Loading...</div>
 
     return (
         <div>
-            {showModal ? (
-                <>
-                    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-                        <div className="relative my-6 mx-auto w-auto max-w-3xl">
-                            {/*content*/}
-                            <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
-                                {/*header*/}
-                                <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
-                                    <h3 className="text-3xl font-semibold">
-                                        Modal Title
-                                    </h3>
-                                    <button
-                                        className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-black opacity-5 outline-none focus:outline-none"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        <span className="block h-6 w-6 bg-transparent text-2xl text-black opacity-5 outline-none focus:outline-none">
-                                            ×
-                                        </span>
-                                    </button>
-                                </div>
-                                {/*body*/}
-                                <div className="relative flex-auto p-6">
-                                    <p className="my-4 text-lg leading-relaxed text-slate-500">
-                                        Modal text
-                                    </p>
-                                    {subscribed ? (
-                                        <button
-                                            className="mr-1 mb-1 rounded bg-red-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-red-600"
-                                            type="button"
-                                            onClick={() => {
-                                                unsubscribe.mutate(
-                                                    {
-                                                        daoId: props.dao.id,
-                                                    },
-                                                    {
-                                                        onSuccess() {
-                                                            setSubscribed(false)
-                                                            setShowModal(false)
-                                                        },
-                                                    }
-                                                )
-                                            }}
-                                        >
-                                            Unsubscribe
-                                        </button>
-                                    ) : (
-                                        <button
-                                            className="mr-1 mb-1 rounded bg-emerald-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
-                                            type="button"
-                                            onClick={() => {
-                                                subscribe.mutate(
-                                                    {
-                                                        daoId: props.dao.id,
-                                                    },
-                                                    {
-                                                        onSuccess() {
-                                                            setSubscribed(true)
-                                                            setShowModal(false)
-                                                        },
-                                                    }
-                                                )
-                                            }}
-                                        >
-                                            Subscribe
-                                        </button>
-                                    )}
-                                </div>
-                                <div className="flex flex-col">
-                                    <div>Last refresh at :</div>
-                                    <div>
-                                        {`${refreshStatus.data.lastRefresh}`}
-                                    </div>
-                                </div>
-                                {refreshStatus.data.refreshStatus == 'DONE' ? (
-                                    <button
-                                        className="mr-1 mb-1 rounded bg-blue-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-blue-600"
-                                        type="button"
-                                        onClick={() => {
-                                            refreshDao.mutate({
-                                                daoId: props.dao.id,
-                                            })
-                                        }}
-                                    >
-                                        Refresh DAO
-                                    </button>
-                                ) : (
-                                    <button
-                                        className="mr-1 mb-1 rounded bg-orange-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-red-600"
-                                        type="button"
-                                    >
-                                        Refresh pending
-                                    </button>
-                                )}
-
-                                {/*footer*/}
-                                <div className="flex items-center justify-end rounded-b border-t border-solid border-slate-200 p-6">
-                                    <button
-                                        className="mr-1 mb-1 px-6 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Close
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
-                </>
-            ) : null}
-            <button
-                onClick={() => setShowModal(true)}
-                type="button"
-                className="mr-1 mb-1 rounded bg-gray-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-500"
-            >
-                <div className="mt-4 flex h-80 w-60 flex-col items-center">
+            <div className="mt-4 mr-1 mb-1 flex h-80 w-60 flex-col items-center justify-between rounded bg-gray-500 text-sm font-bold text-white shadow">
+                <div className="flex flex-col items-center pt-5">
                     <Image
                         width="96"
                         height="96"
@@ -194,7 +65,25 @@ export const UnfollowedDAO = (props: {
                         })}
                     </div>
                 </div>
-            </button>
+                <button
+                    className="h-20 w-full bg-gray-300 text-xl font-bold text-black"
+                    onClick={() => {
+                        subscribe.mutate(
+                            {
+                                daoId: props.dao.id,
+                            },
+                            {
+                                onSuccess() {
+                                    refreshStatus.refetch()
+                                    props.refreshDaos()
+                                },
+                            }
+                        )
+                    }}
+                >
+                    Subscribe
+                </button>
+            </div>
         </div>
     )
 }
