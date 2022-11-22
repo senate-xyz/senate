4a588068d989993e8bec10ba4b72f45b24d2708f
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-11-23 01:56:31 +0200

    cypress e2e testing

---
+++ apps/web/src/pages/about.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <main>
+                <h1>About Page</h1>
+            </main>
+        </div>
+    )
+}
+
+export default About
--- packages/config/eslint.js
+++ packages/config/eslint.js
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:promise/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:import/recommended",
+    "plugin:cypress/recommended",
     "prettier",
   ],
 
