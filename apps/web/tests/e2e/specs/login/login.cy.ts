fe8da1de99adfc3668b87ab843f5937316626917
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-11-23 15:35:41 +0200

    more login tests

--- apps/web/tests/e2e/specs/login/login.cy.ts
+++ apps/web/tests/e2e/specs/login/login.cy.ts
@@ -1,12 +1,20 @@
 /// <reference types="cypress" />
-import '@synthetixio/synpress/support/commands'
 
 describe('home', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/dashboard/daos')
-    })
 
-    it('should have Senate title', () => {
+        cy.setupMetamask(
+            'test test test test test test test test test test test junk',
+            'mainnet',
+            'senate123'
+        )
+    })
+    it('it should do wallet connect on clear cookies', () => {
+        cy.clearCookies()
+
+        cy.disconnectMetamaskWalletFromAllDapps()
+
         cy.get('[data-cy="daos"]')
             .get('[data-cy="dashboard-header"]')
             .contains('Connect Wallet')
@@ -14,18 +22,51 @@ describe('home', () => {
 
         cy.contains('MetaMask').click()
 
-        cy.contains('Send message').click()
-
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(1000)
-
         cy.switchToMetamaskNotification()
+        cy.acceptMetamaskAccess()
 
+        cy.switchToCypressWindow()
+
+        cy.contains('Send message').click()
+        cy.switchToMetamaskNotification()
         cy.confirmMetamaskSignatureRequest()
 
         cy.switchToCypressWindow()
 
         cy.contains('0xf3…2266')
+    })
+
+    it('it should connect with cookies', () => {
+        cy.get('[data-cy="daos"]')
+            .get('[data-cy="dashboard-header"]')
+            .contains('Connect Wallet')
+            .click()
+        cy.contains('Send message').click()
+        cy.switchToMetamaskNotification()
+        cy.confirmMetamaskSignatureRequest()
+
+        cy.switchToCypressWindow()
+
+        cy.contains('0xf3…2266')
+    })
+
+    it('it should disconnect with cookies', () => {
+        cy.get('[data-cy="daos"]')
+            .get('[data-cy="dashboard-header"]')
+            .contains('Connect Wallet')
+            .click()
+        cy.contains('Send message').click()
+        cy.switchToMetamaskNotification()
+        cy.confirmMetamaskSignatureRequest()
+
+        cy.switchToCypressWindow()
+
+        cy.contains('0xf3…2266').click()
+        cy.contains('Disconnect').click()
+
+        cy.get('[data-cy="daos"]')
+            .get('[data-cy="dashboard-header"]')
+            .contains('Connect Wallet')
     })
 })
 
--- apps/web/tests/e2e/support.js
+++ apps/web/tests/e2e/support.js
@@ -1,9 +1,1 @@
-import * as path from 'path'
-const synpressPath = path.join(
-    process.cwd(),
-    '/node_modules/@synthetixio/synpress'
-)
-
-module.exports = {
-    extends: `${synpressPath}/.eslintrc.js`,
-}
+import '@synthetixio/synpress/support/commands'
