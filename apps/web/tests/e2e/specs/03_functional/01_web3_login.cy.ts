45272d8bfda1b39394877b27dd98f1091a5736f8
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-12-01 23:22:28 +0200

    Revert "Update config.yml"
    This reverts commit 2ebc95de6332d784590791e17206332480930dfb.

--- apps/web/tests/e2e/specs/03_functional/01_web3_login.cy.ts
+++ apps/web/tests/e2e/specs/03_functional/01_web3_login.cy.ts
@@ -7,10 +7,10 @@ describe('login', () => {
 
     it('it should do wallet connect', () => {
         /* ==== Generated with Cypress Studio ==== */
-        cy.get('[data-testid="rk-connect-button"]').click()
-        cy.get('[data-testid="rk-wallet-option-metaMask"]').click()
+        cy.contains('Connect Wallet').click()
+        cy.contains('MetaMask').click()
         cy.acceptMetamaskAccess()
-        cy.get('[data-testid="rk-auth-message-button"]').click()
+        cy.contains('Send message').click()
         cy.confirmMetamaskSignatureRequest()
         cy.get('[data-testid="rk-account-button"]').should(
             'have.text',
@@ -21,8 +21,8 @@ describe('login', () => {
 
     it('it should do wallet connect again', () => {
         /* ==== Generated with Cypress Studio ==== */
-        cy.get('[data-testid="rk-connect-button"]').click()
-        cy.get('[data-testid="rk-auth-message-button"]').click()
+        cy.contains('Connect Wallet').click()
+        cy.contains('Send message').click()
         cy.confirmMetamaskSignatureRequest()
         cy.get('[data-testid="rk-account-button"]').should(
             'have.text',
