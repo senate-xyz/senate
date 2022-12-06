9cd99cf67c266e58f63344ff2504510974c9039a
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-12-07 01:14:07 +0200

    updated project icons

--- apps/web/src/components/Dashboard.tsx
+++ apps/web/src/components/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = (props: { title: string; component: JSX.Element }) => {
         <div className="h-full min-h-screen w-full bg-black">
             <div className="flex w-full flex-col">
                 <div className="flex h-48 items-center justify-between border border-x-0 border-t-0 border-[#545454] px-10">
-                    <h1 className="text-[78px] text-white font-semibold">
+                    <h1 className="text-[78px] font-semibold text-white">
                         {props.title}
                     </h1>
                     <RainbowConnect />
