f18750cd42cdd4a4ab23786b9e83afe2f131e687
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-12-15 11:31:05 +0200

    Update Dashboard.tsx

--- apps/web/src/components/Dashboard.tsx
+++ apps/web/src/components/Dashboard.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react'
 import RainbowConnect from './RainbowConnect'
 
 const Dashboard = (props: { title: string; component: JSX.Element }) => {
+    const [headerHeight, setHeaderHeight] = useState('h-[192px]')
+    const [componentPadding, setComponentPadding] = useState('pt-[192px]')
+
     return (
-        <div className="h-full min-h-screen w-full bg-black">
-            <div className="flex h-full min-h-screen w-full flex-col">
-                <div className="flex h-48 items-center justify-between border border-x-0 border-t-0 border-[#545454] px-10">
-                    <h1 className="text-[78px] font-semibold text-white">
+        <div
+            className="h-full min-h-screen w-full bg-black"
+            onWheel={(e) => {
+                console.log(window.scrollY)
+                if (window.scrollY > 0) {
+                    setHeaderHeight('h-[96px]')
+                    setComponentPadding('pt-[96px]')
+                } else {
+                    setHeaderHeight('h-[192px]')
+                    setComponentPadding('pt-[192px]')
+                }
+            }}
+        >
+            <div className="flex h-full min-h-screen flex-col">
+                <div
+                    className={`fixed z-10 flex w-full bg-black transition-all ${headerHeight} items-center justify-between border border-x-0 border-t-0 border-[#545454] px-10`}
+                >
+                    <h1 className={`text-[78px] font-semibold text-white`}>
                         {props.title}
                     </h1>
-                    <RainbowConnect />
+                    <div className="pr-20">
+                        <RainbowConnect />
+                    </div>
                 </div>
-                {props.component}
+                <div className={`${componentPadding}`}>{props.component}</div>
             </div>
         </div>
     )
