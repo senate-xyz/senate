afa9c2f99c24822ef2e4191d89f89416990ea3fa
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-12-19 22:32:34 +0200

    title size

--- apps/web/src/components/Dashboard.tsx
+++ apps/web/src/components/Dashboard.tsx
@@ -5,17 +5,23 @@ import Link from 'next/link'
 const Dashboard = (props: { title: string; component: JSX.Element }) => {
     const [headerHeight, setHeaderHeight] = useState('h-[192px]')
     const [componentPadding, setComponentPadding] = useState('pt-[192px]')
+    const [titleSize, setTitleSize] = useState('text-[78px]')
 
     return (
         <div
             className="h-full min-h-screen w-full bg-black"
             onWheel={() => {
+                console.log(window.scrollY)
                 if (window.scrollY > 0) {
                     setHeaderHeight('h-[96px]')
                     setComponentPadding('pt-[96px]')
                 } else {
                     setHeaderHeight('h-[192px]')
                     setComponentPadding('pt-[192px]')
+                    setTitleSize('text-[78px]')
+                }
+                if (window.scrollY > 13) {
+                    setTitleSize('text-[52px]')
                 }
             }}
         >
@@ -36,7 +42,7 @@ const Dashboard = (props: { title: string; component: JSX.Element }) => {
                 <div
                     className={`fixed z-10 flex w-full bg-black transition-all ${headerHeight} items-center justify-between border border-x-0 border-t-0 border-[#545454] px-10`}
                 >
-                    <h1 className={`text-[78px] font-semibold text-white`}>
+                    <h1 className={`${titleSize} font-semibold text-white`}>
                         {props.title}
                     </h1>
                     <div className="pr-20">
