32b261a2f85abffd49e72fa2eb9e1cce5a0cc2c6
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-01-10 20:29:21 +0200

    more scripts

--- apps/refresher/tsup.config.ts
+++ apps/refresher/tsup.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     clean: true,
     dts: true,
     entry: ['src/index.ts'],
-    format: ['esm'],
+    format: ['cjs', 'esm'],
     minify: isProduction,
     sourcemap: true
 })
