5ff2c3e6ed8f07492886883a0196feee811a1069
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-01-11 00:37:50 +0200

    Revert "Update index.ts"
    This reverts commit b80bb99c27e31986daee40010112773aaa145b59.

--- apps/refresher/src/index.ts
+++ apps/refresher/src/index.ts
@@ -34,43 +34,43 @@ const main = async () => {
                     await prisma.$metrics.json()
                 ).counters.find(
                     (metric) => metric.key == 'prisma_client_queries_total'
-                ),
+                )?.value,
                 prisma_datasource_queries_total: (
                     await prisma.$metrics.json()
                 ).counters.find(
                     (metric) => metric.key == 'prisma_datasource_queries_total'
-                ),
+                )?.value,
                 prisma_pool_connections_open: (
                     await prisma.$metrics.json()
                 ).counters.find(
                     (metric) => metric.key == 'prisma_pool_connections_open'
-                ),
+                )?.value,
                 prisma_client_queries_active: (
                     await prisma.$metrics.json()
                 ).gauges.find(
                     (metric) => metric.key == 'prisma_client_queries_active'
-                ),
+                )?.value,
                 prisma_client_queries_wait: (
                     await prisma.$metrics.json()
                 ).gauges.find(
                     (metric) => metric.key == 'prisma_client_queries_wait'
-                ),
+                )?.value,
                 prisma_pool_connections_busy: (
                     await prisma.$metrics.json()
                 ).gauges.find(
                     (metric) => metric.key == 'prisma_pool_connections_busy'
-                ),
+                )?.value,
                 prisma_pool_connections_idle: (
                     await prisma.$metrics.json()
                 ).gauges.find(
                     (metric) => metric.key == 'prisma_pool_connections_idle'
-                ),
+                )?.value,
                 prisma_pool_connections_opened_total: (
                     await prisma.$metrics.json()
                 ).gauges.find(
                     (metric) =>
                         metric.key == 'prisma_pool_connections_opened_total'
-                )
+                )?.value
             }
         })
         await loadConfig()
