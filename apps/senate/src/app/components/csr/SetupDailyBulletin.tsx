e55042bc1c708ed9379f20bdc9be3f2e6d70dc63
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-03-08 13:01:37 +0200

    more responsive

--- apps/senate/src/app/components/csr/SetupDailyBulletin.tsx
+++ apps/senate/src/app/components/csr/SetupDailyBulletin.tsx
@@ -5,14 +5,14 @@ import { useRouter } from 'next/navigation'
 const SetupDailyBulletin = () => {
     const router = useRouter()
     return (
-        <div className='flex h-[72px] w-full flex-row items-center justify-between bg-[#FFF1BF] p-6'>
+        <div className='flex w-full flex-row items-center justify-between gap-2 bg-[#FFF1BF] p-6 lg:h-[72px]'>
             <div className='text-[24px] font-light text-black'>
                 You can setup a daily email to notify you of the latest
                 proposals for the DAOs that you’re subscribed to on Senate.
             </div>
 
             <button
-                className='flex h-[44px] w-[208px] items-center justify-center bg-black text-white'
+                className='flex items-center justify-center bg-black p-2 text-white lg:h-[44px] lg:w-[208px] lg:p-0'
                 onClick={() => {
                     router.push('/bulletin')
                 }}
