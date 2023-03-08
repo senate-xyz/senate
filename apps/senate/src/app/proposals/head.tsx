3a6a0468fa8577840a62a5ef7c7ee19fcb89aa3f
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-03-08 17:56:32 +0200

    more howuku

--- apps/senate/src/app/proposals/head.tsx
+++ apps/senate/src/app/proposals/head.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 export default function Head() {
     return (
         <>
@@ -13,6 +15,15 @@ export default function Head() {
                 name='viewport'
             />
             <link rel='icon' href='/favicon.ico' />
+
+            <Script id='howuku'>
+                {`(function(t,r,a,c,k){
+                c=['track','identify','converted'],t.o=t._init||{},
+                c.map(function(n){return t.o[n]=t.o[n]||function(){(t.o[n].q=t.o[n].q||[]).push(arguments);};}),t._init=t.o,
+                k=r.createElement("script"),k.type="text/javascript",k.async=true,k.src="https://cdn.howuku.com/js/track.js",k.setAttribute("key",a),
+                r.getElementsByTagName("head")[0].appendChild(k);
+                })(window, document, "9mv6yAGkYDZV0BJEzlN34O");`}
+            </Script>
         </>
     )
 }
