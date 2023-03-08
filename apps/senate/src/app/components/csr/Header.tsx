bcbbff0f5a1330ba12d324502fd43b0df0797028
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-03-08 02:58:20 +0200

    more

--- apps/senate/src/app/components/csr/Header.tsx
+++ apps/senate/src/app/components/csr/Header.tsx
@@ -1,34 +1,48 @@
 'use client'
 
-import { Fragment, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import WalletConnect from './WalletConnect'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { Menu, Transition } from '@headlessui/react'
 
 export const Header = (props: { title: string }) => {
-    const [headerHeight, setHeaderHeight] = useState('h-[192px]')
-    const [titleSize, setTitleSize] = useState('text-[78px]')
+    const [headerHeight, setHeaderHeight] = useState('lg:h-[192px]')
+    const [titleSize, setTitleSize] = useState('lg:text-[78px]')
 
-    if (typeof window != 'undefined')
+    if (typeof window != 'undefined') {
         window.addEventListener('wheel', () => {
             if (
                 window.scrollY > 0 &&
                 document.body.scrollHeight > window.innerHeight + 100
             ) {
-                setHeaderHeight('h-[96px]')
-                setTitleSize('text-[52px]')
+                setHeaderHeight('lg:h-[96px]')
+                setTitleSize('lg:text-[52px]')
             } else {
-                setHeaderHeight('h-[192px]')
-                setTitleSize('text-[78px]')
+                setHeaderHeight('lg:h-[192px]')
+                setTitleSize('lg:text-[78px]')
             }
         })
+    }
+
+    useEffect(() => {
+        if (
+            window.scrollY > 0 &&
+            document.body.scrollHeight > window.innerHeight + 100
+        ) {
+            setHeaderHeight('lg:h-[96px]')
+            setTitleSize('lg:text-[52px]')
+        } else {
+            setHeaderHeight('lg:h-[192px]')
+            setTitleSize('lg:text-[78px]')
+        }
+    }, [])
 
     return (
         <div
-            className={`lg:${headerHeight} fixed z-20 flex h-[96px] w-full items-center justify-between border border-x-0 border-t-0 border-[#545454] bg-black px-10 transition-all`}
+            className={`${headerHeight} fixed z-20 flex h-[96px] w-full items-center justify-between border border-x-0 border-t-0 border-[#545454] bg-black px-10 transition-all`}
         >
             <h1
-                className={`lg:${titleSize} text-[52px] font-extrabold text-white transition`}
+                className={`${titleSize} text-[52px] font-extrabold text-white transition`}
             >
                 {props.title}
             </h1>
