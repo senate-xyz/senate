653a9aaf5b0a06883ccdbbded54e6822b289cfa1
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-03-21 00:53:06 +0200

    cleaner

--- apps/detective/src/utils.ts
+++ apps/detective/src/utils.ts
@@ -10,55 +10,39 @@ const getAbi = async (address: string, provider: 'ethereum' | 'arbitrum') => {
         })
         .catch(async () => {
             const result = { abi: 'none' }
-            if (provider === 'ethereum') {
-                await superagent
-                    .get(
-                        `https://api.etherscan.io/api?module=contract&action=getabi&address=${address}&apikey=${process.env.ETHERSCAN_API_KEY}`
-                    )
-                    .type('application/json')
-                    .retry(3, async (err, res) => {
-                        if (res.status === 201) return false
-                        if (err) {
-                            await new Promise((f) => setTimeout(f, 1000))
-                            return true
+
+            await superagent
+                .get(
+                    `${
+                        provider === 'ethereum'
+                            ? 'https://api.etherscan.io'
+                            : 'https://api.arbiscan.io'
+                    }/api?module=contract&action=getabi&address=${address}&apikey=${
+                        provider === 'ethereum'
+                            ? process.env.ETHERSCAN_API_KEY
+                            : process.env.ARBISCAN_API_KEY
+                    }`
+                )
+                .type('application/json')
+                .retry(3, async (err, res) => {
+                    if (res.status === 201) return false
+                    if (err) {
+                        await new Promise((f) => setTimeout(f, 1000))
+                        return true
+                    }
+                    return false
+                })
+                .then((res) => res.body)
+                .then(async (data) => {
+                    await prisma.contract.create({
+                        data: {
+                            address: address,
+                            abi: data.result
                         }
-                        return false
                     })
-                    .then((res) => res.body)
-                    .then(async (data) => {
-                        await prisma.contract.create({
-                            data: {
-                                address: address,
-                                abi: data.result
-                            }
-                        })
-                        result.abi = data.result
-                    })
-            } else if (provider === 'arbitrum') {
-                await superagent
-                    .get(
-                        `https://api.arbiscan.io/api?module=contract&action=getabi&address=${address}&apikey=${process.env.ARBISCAN_API_KEY}`
-                    )
-                    .type('application/json')
-                    .retry(3, async (err, res) => {
-                        if (res.status === 201) return false
-                        if (err) {
-                            await new Promise((f) => setTimeout(f, 1000))
-                            return true
-                        }
-                        return false
-                    })
-                    .then((res) => res.body)
-                    .then(async (data) => {
-                        await prisma.contract.create({
-                            data: {
-                                address: address,
-                                abi: data.result
-                            }
-                        })
-                        result.abi = data.result
-                    })
-            }
+                    result.abi = data.result
+                })
+
             return result
         })
 
