ad6bf55b547ae1a312534e1646e25e58049015ec
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-03-23 20:37:10 +0200

    Update index.ts

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,11 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 import type { InterfaceAbi } from 'ethers'
 
+declare global {
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient | undefined
+}
+
 export type { JsonArray, JsonValue } from 'type-fest'
 
 export {
@@ -65,11 +70,6 @@ export type UserWithVotingAddresses = Prisma.UserGetPayload<{
         voters: true
     }
 }>
-
-declare global {
-    // eslint-disable-next-line no-var
-    var prisma: PrismaClient | undefined
-}
 
 export const prisma = global.prisma || new PrismaClient()
 
