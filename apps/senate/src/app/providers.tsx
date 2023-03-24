4700be09535941164fbc394e4afdcfbb26bbbca2
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-03-24 13:46:01 +0200

    Update providers.tsx

--- apps/senate/src/app/providers.tsx
+++ apps/senate/src/app/providers.tsx
@@ -1,7 +1,8 @@
 import {
-    getDefaultWallets,
     RainbowKitProvider,
-    darkTheme
+    darkTheme,
+    DisclaimerComponent,
+    getDefaultWallets
 } from '@rainbow-me/rainbowkit'
 import {
     type GetSiweMessageOptions,
@@ -11,6 +12,7 @@ import { SessionProvider } from 'next-auth/react'
 import { configureChains, mainnet, createClient, WagmiConfig } from 'wagmi'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { TrpcClientProvider } from '../server/trpcClient'
+import Link from 'next/link'
 
 const { chains, provider } = configureChains(
     [mainnet],
@@ -38,6 +40,41 @@ const getSiweMessageOptions: GetSiweMessageOptions = () => ({
     statement: 'Sign in to Senate'
 })
 
+const Disclaimer: DisclaimerComponent = () => (
+    <div className='ml-2 select-none text-sm font-light text-white'>
+        By connecting your wallet, you agree to the{' '}
+        <Link
+            href={
+                'https://senate.notion.site/Senate-Labs-Terms-of-Service-990ca9e655094b6f9673a3ead572956a'
+            }
+            className='underline'
+            target='_blank'
+        >
+            Terms of Service
+        </Link>
+        ,{' '}
+        <Link
+            href={
+                'https://senate.notion.site/Senate-Labs-Privacy-Policy-494e23d8a4e34d0189bfe07e0ae01bde'
+            }
+            className='underline'
+            target='_blank'
+        >
+            Privacy Policy
+        </Link>{' '}
+        and{' '}
+        <Link
+            href={
+                'https://senate.notion.site/Senate-Labs-Cookie-Policy-b429fe7b181e4cfda95f404f480bfdc7'
+            }
+            className='underline'
+            target='_blank'
+        >
+            Cookie Policy
+        </Link>{' '}
+    </div>
+)
+
 export default function RootProvider({
     children
 }: {
@@ -50,6 +87,11 @@ export default function RootProvider({
                     getSiweMessageOptions={getSiweMessageOptions}
                 >
                     <RainbowKitProvider
+                        appInfo={{
+                            appName: 'Senate',
+                            disclaimer: Disclaimer,
+                            learnMoreUrl: 'https://senate.notion.site'
+                        }}
                         chains={chains}
                         modalSize='compact'
                         theme={darkTheme({
