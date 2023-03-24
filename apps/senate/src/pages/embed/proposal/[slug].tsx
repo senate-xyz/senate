eee5f4092e5d49851b7f7735cb0b4c4ef2d71df5
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-03-24 19:13:11 +0200

    prettier

--- apps/senate/src/pages/embed/proposal/[slug].tsx
+++ apps/senate/src/pages/embed/proposal/[slug].tsx
@@ -3,6 +3,8 @@ import { TrpcClientProvider, trpc } from '../../../server/trpcClient'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { RouterOutputs } from '../../../server/trpc'
+import Image from 'next/image'
+import '../../../styles/globals.css'
 
 dayjs.extend(relativeTime)
 
@@ -35,14 +37,126 @@ const EmbeddedProposal = () => {
 
 const Proposal = (props: { proposal: RouterOutputs['public']['proposal'] }) => {
     return (
-        <div className='flex flex-col'>
-            <div>{props.proposal?.dao.name}</div>
-            <div>{props.proposal?.name}</div>
-            <div>{props.proposal?.timeStart.toISOString()}</div>
-            <div>{props.proposal?.timeEnd.toISOString()}</div>
-            <div>{props.proposal?.quorum}</div>
-            <div>{props.proposal?.choices?.toString()}</div>
-            <div>{props.proposal?.scores?.toString()}</div>
+        <div className='my-1 flex w-full flex-col items-start bg-[#121212] text-[#EDEDED]'>
+            <div className='flex w-full flex-col gap-2 p-2'>
+                <div className='flex flex-row gap-2'>
+                    <div className='flex flex-col items-center justify-start gap-2'>
+                        <div className='w-[48px] border border-b-2 border-r-2 border-t-0 border-l-0'>
+                            <Image
+                                loading='eager'
+                                priority={true}
+                                width={48}
+                                height={48}
+                                src={props.proposal.daoPicture + '_medium.png'}
+                                alt={props.proposal.daoName}
+                            />
+                        </div>
+
+                        <div>
+                            {props.proposal.onchain ? (
+                                <Image
+                                    loading='eager'
+                                    priority={true}
+                                    width={50}
+                                    height={15}
+                                    src={'/assets/Icon/OnChainProposal.svg'}
+                                    alt='off-chain'
+                                />
+                            ) : (
+                                <Image
+                                    loading='eager'
+                                    priority={true}
+                                    width={50}
+                                    height={14}
+                                    src={'/assets/Icon/OffChainProposal.svg'}
+                                    alt='off-chain'
+                                />
+                            )}
+                        </div>
+                    </div>
+                    <div className='cursor-pointer self-center pb-5 hover:underline'>
+                        <a
+                            href={
+                                props.proposal.proposalLink.includes(
+                                    'snapshot.org'
+                                )
+                                    ? props.proposal.proposalLink +
+                                      '?app=senate'
+                                    : props.proposal.proposalLink
+                            }
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            <div className='text-[15px] font-normal leading-[23px]'>
+                                {props.proposal.proposalTitle}
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div className='flex w-full flex-row items-end justify-between'>
+                    <div className='flex w-3/4 flex-col justify-between'>
+                        <div>
+                            {props.proposal.highestScoreChoice !=
+                            'undefined' ? (
+                                <div>
+                                    <div className='flex flex-row'>
+                                        <div className='text-[21px] leading-[26px] text-white'>
+                                            {props.proposal.highestScoreChoice}
+                                        </div>
+                                    </div>
+                                    <div className='bg-[#262626]'>
+                                        <div
+                                            style={{
+                                                width: `${(
+                                                    (props.proposal
+                                                        .highestScore /
+                                                        props.proposal
+                                                            .scoresTotal) *
+                                                    100
+                                                ).toFixed(0)}%`
+                                            }}
+                                            className={`h-full bg-[#EDEDED]`}
+                                        >
+                                            <div className='px-2 text-black'>
+                                                {(
+                                                    (props.proposal
+                                                        .highestScore /
+                                                        props.proposal
+                                                            .scoresTotal) *
+                                                    100
+                                                ).toFixed(2)}
+                                                %
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className='text-[17px] leading-[26px] text-white'>
+                                    Unable to fetch results data
+                                </div>
+                            )}
+                        </div>
+                        <div className='text-[12px] font-normal leading-[19px]'>
+                            {`on ${new Date(
+                                props.proposal.timeEnd
+                            ).toLocaleDateString('en-US', {
+                                month: 'long',
+                                day: 'numeric'
+                            })} at ${new Date(
+                                props.proposal.timeEnd
+                            ).toLocaleTimeString('en-US', {
+                                hour: '2-digit',
+                                minute: '2-digit',
+                                second: '2-digit',
+                                timeZone: 'UTC',
+                                hour12: false
+                            })} UTC
+                    `}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- apps/senate/src/server/routers/public.ts
+++ apps/senate/src/server/routers/public.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 import { publicProcedure, router } from '../trpc'
-import { prisma } from '@senate/database'
+import { JsonArray, prisma } from '@senate/database'
 
 export const publicRouter = router({
     allDAOs: publicProcedure.query(async () => {
@@ -15,15 +15,55 @@ export const publicRouter = router({
             })
         )
         .query(async ({ input }) => {
-            const proposal = await prisma.proposal.findFirst({
+            const proposal = await prisma.proposal.findFirstOrThrow({
                 where: {
                     id: input.id
                 },
                 include: {
-                    dao: true
+                    dao: true,
+                    daoHandler: true
                 }
             })
 
-            return proposal
+            let highestScore = 0
+            let highestScoreIndex = 0
+            let highestScoreChoice = ''
+
+            if (
+                proposal.scores &&
+                typeof proposal.scores === 'object' &&
+                Array.isArray(proposal?.scores) &&
+                proposal.choices &&
+                typeof proposal.choices === 'object' &&
+                Array.isArray(proposal?.choices)
+            ) {
+                const scores = proposal.scores as JsonArray
+
+                for (const score of scores) {
+                    if (Number(score) > highestScore) {
+                        highestScore = Number(score)
+                        highestScoreIndex++
+                    }
+                }
+
+                highestScoreChoice = String(
+                    proposal.choices[highestScoreIndex - 1]
+                )
+            }
+
+            const result = {
+                daoName: proposal.dao.name,
+                onchain: proposal.daoHandler.type == 'SNAPSHOT' ? false : true,
+                daoPicture: proposal.dao.picture,
+                proposalTitle: proposal.name,
+                proposalLink: proposal.url,
+                timeEnd: proposal.timeEnd,
+                highestScoreChoice: highestScoreChoice,
+                highestScore: highestScore,
+                scoresTotal: proposal.scoresTotal,
+                passedQuorum: proposal.quorum < proposal.scoresTotal
+            }
+
+            return result
         })
 })
