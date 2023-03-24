84b0ce722c5cbebad0b8fa7b016a2ff7424515a6
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-03-24 22:36:00 +0200

    allow other origins for nextauth

--- apps/senate/src/pages/api/auth/[...nextauth].ts
+++ apps/senate/src/pages/api/auth/[...nextauth].ts
@@ -81,6 +81,34 @@ export function authOptions(
                 return session
             }
         },
+        cookies: {
+            sessionToken: {
+                name: `__Secure-next-auth.session-token`,
+                options: {
+                    path: '/',
+                    httpOnly: true,
+                    sameSite: 'none',
+                    secure: true
+                }
+            },
+            callbackUrl: {
+                name: `__Secure-next-auth.callback-url`,
+                options: {
+                    path: '/',
+                    sameSite: 'none',
+                    secure: true
+                }
+            },
+            csrfToken: {
+                name: `__Host-next-auth.csrf-token`,
+                options: {
+                    path: '/',
+                    httpOnly: true,
+                    sameSite: 'none',
+                    secure: true
+                }
+            }
+        },
         secret: process.env.NEXTAUTH_SECRET,
         events: {
             async signIn(message) {
