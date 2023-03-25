0028dedd958c4d644b1ba214aac8c852d56c2954
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-03-25 19:04:30 +0200

    Revert "add scout"
    This reverts commit 8d9e8067e17a38fe1651861c4a1e23720cedc4e4.

--- apps/refresher/src/index.ts
+++ apps/refresher/src/index.ts
@@ -12,22 +12,12 @@ import { addSnapshotProposalsToQueue } from './populate/addSnapshotProposals'
 import { sleep } from './utils'
 import { scheduleJob } from 'node-schedule'
 //import { log_ref } from '@senate/axiom'
-import scout from '@scout_apm/scout-apm'
 
 const main = async () => {
     // log_ref.log({
     //     level: 'info',
     //     message: `Started refresher`
     // })
-
-    await scout.install({
-        monitor: true, // enable monitoring
-        name: 'refresher',
-        key: 'Qrrm787sLcZ7xcLDOi4L',
-
-        // allow scout to be shutdown when the process exits
-        allowShutdown: true
-    })
 
     await loadConfig()
     await createVoterHandlers()
