38c79116097a755eee6ee825ab4ab95fcd91b959
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-04-02 16:10:22 +0300

    bring back to monorepo, should work with a dockerfile

---
+++ apps/refresher-rust/src/create_queue/snapshot_votes.rs
@@ -0,0 +1,193 @@
+use std::collections::HashMap;
+
+use crate::{ prisma::{ self, voterhandler }, RefreshEntry, RefreshType, config::Config };
+
+use prisma::{ PrismaClient, daohandler };
+use prisma_client_rust::{ chrono::{ Utc, Duration }, operator::{ or, and } };
+
+pub async fn get_snapshot_votes_queue(client: &PrismaClient, config: &Config) -> Vec<RefreshEntry> {
+    let normal_refresh = Utc::now() - Duration::milliseconds(config.normal_snapshot_votes.into());
+    let force_refresh = Utc::now() - Duration::milliseconds(config.force_snapshot_votes.into());
+    let new_refresh = Utc::now() - Duration::milliseconds(config.new_snapshot_votes.into());
+
+    let dao_handlers = client
+        .daohandler()
+        .find_many(
+            vec![
+                daohandler::r#type::equals(prisma::DaoHandlerType::Snapshot),
+                daohandler::voterhandlers::some(
+                    vec![
+                        or(
+                            vec![
+                                and(
+                                    vec![
+                                        voterhandler::refreshstatus::equals(
+                                            prisma::RefreshStatus::Done
+                                        ),
+                                        voterhandler::lastrefresh::lt(normal_refresh.into())
+                                    ]
+                                ),
+                                and(
+                                    vec![
+                                        voterhandler::refreshstatus::equals(
+                                            prisma::RefreshStatus::Pending
+                                        ),
+                                        voterhandler::lastrefresh::lt(force_refresh.into())
+                                    ]
+                                ),
+                                and(
+                                    vec![
+                                        voterhandler::refreshstatus::equals(
+                                            prisma::RefreshStatus::New
+                                        ),
+                                        voterhandler::lastrefresh::lt(new_refresh.into())
+                                    ]
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
+        .include(
+            daohandler::include!({ 
+                    dao 
+                    proposals 
+                    voterhandlers: 
+                        include { voter }
+                })
+        )
+        .exec().await
+        .unwrap();
+
+    let mut voter_handler_to_refresh = Vec::new();
+
+    let filtered_dao_handlers: Vec<_> = dao_handlers
+        .into_iter()
+        .filter(|dao_handler| !dao_handler.proposals.is_empty())
+        .collect();
+
+    let refresh_queue: Vec<RefreshEntry> = filtered_dao_handlers
+        .iter()
+        .cloned()
+        .flat_map(|dao_handler| {
+            let voter_handlers: Vec<_> = dao_handler.voterhandlers
+                .iter()
+                .cloned()
+                .filter(
+                    |voter_handler|
+                        (voter_handler.refreshstatus == prisma::RefreshStatus::Done &&
+                            voter_handler.lastrefresh.lt(&normal_refresh)) ||
+                        (voter_handler.refreshstatus == prisma::RefreshStatus::Pending &&
+                            voter_handler.lastrefresh.lt(&force_refresh)) ||
+                        (voter_handler.refreshstatus == prisma::RefreshStatus::New &&
+                            voter_handler.lastrefresh.lt(&new_refresh))
+                )
+                .collect();
+
+            let vote_indexes: Vec<i64> = voter_handlers
+                .iter()
+                .cloned()
+                .map(|voter_handler| voter_handler.snapshotindex.unwrap().timestamp_millis())
+                .collect();
+
+            let vote_indexes_buckets = bin(vote_indexes, 10);
+
+            let refresh_items: Vec<RefreshEntry> = vote_indexes_buckets
+                .iter()
+                .filter_map(|bucket| {
+                    let bucket_vh: Vec<_> = voter_handlers
+                        .iter()
+                        .cloned()
+                        .filter(|voter_handler| {
+                            let index = voter_handler.snapshotindex.unwrap().timestamp_millis();
+                            bucket.min <= index && index <= bucket.max
+                        })
+                        .take(config.batch_snapshot_votes.try_into().unwrap())
+                        .collect();
+
+                    voter_handler_to_refresh.extend(bucket_vh.clone());
+
+                    if bucket_vh.is_empty() {
+                        None
+                    } else {
+                        Some(RefreshEntry {
+                            handler_id: dao_handler.id.clone(),
+                            refresh_type: RefreshType::Daosnapshotvotes,
+                            voters: bucket_vh
+                                .iter()
+                                .map(|vhandler| vhandler.voter.address.clone())
+                                .collect(),
+                        })
+                    }
+                })
+                .collect();
+
+            refresh_items
+        })
+        .collect();
+
+    let _updated_voter_handlers = client
+        .voterhandler()
+        .update_many(
+            vec![
+                voterhandler::id::in_vec(
+                    voter_handler_to_refresh
+                        .iter()
+                        .map(|vhandler| vhandler.id.clone())
+                        .collect()
+                )
+            ],
+            vec![
+                voterhandler::refreshstatus::set(prisma::RefreshStatus::Pending),
+                voterhandler::lastrefresh::set(Utc::now().into())
+            ]
+        )
+        .exec().await;
+
+    //println!("Added {:?} snapshot voter requests to queue", updated_voter_handlers.unwrap());
+
+    refresh_queue
+}
+
+#[derive(Debug)]
+struct Bucket {
+    min: i64,
+    max: i64,
+}
+
+fn bin(data: Vec<i64>, num_bins: usize) -> Vec<Bucket> {
+    let min = data.iter().cloned().min().unwrap_or(0);
+    let max = data.iter().cloned().max().unwrap_or(0) + 1;
+
+    let bin_width = ((max - min) as f64) / (num_bins as f64);
+    let mut bins: Vec<Bucket> = Vec::with_capacity(num_bins);
+
+    for i in 0..num_bins {
+        bins.push(Bucket {
+            min: min + (((i as f64) * bin_width).round() as i64),
+            max: min + ((((i + 1) as f64) * bin_width).round() as i64),
+        });
+    }
+
+    let mut data_counts: HashMap<usize, u64> = HashMap::new();
+
+    for value in data {
+        let bin_index = (((value - min) as f64) / bin_width).floor() as usize;
+        let count = data_counts.entry(bin_index).or_insert(0);
+        *count += 1;
+    }
+
+    let mut filled_bins: Vec<Bucket> = Vec::with_capacity(num_bins);
+
+    for (bin_index, count) in data_counts.iter() {
+        if *count > 0 {
+            filled_bins.push(Bucket {
+                min: bins[*bin_index].min,
+                max: bins[*bin_index].max,
+            });
+        }
+    }
+
+    filled_bins
+}
