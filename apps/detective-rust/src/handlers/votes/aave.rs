8dc7c4d6b7fae44c5c23b561c25a2e98e6ecab36
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-04-06 10:33:36 +0300

    lint

--- apps/detective-rust/src/handlers/votes/aave.rs
+++ apps/detective-rust/src/handlers/votes/aave.rs
@@ -2,10 +2,10 @@ use anyhow::Result;
 use crate::{ Ctx, prisma::daohandler, router::update_chain_votes::VoteResult };
 
 pub async fn aave_votes(
-    ctx: &Ctx,
-    dao_handler: &daohandler::Data,
-    from_block: &i64,
-    to_block: &i64,
+    _ctx: &Ctx,
+    _dao_handler: &daohandler::Data,
+    _from_block: &i64,
+    _to_block: &i64,
     voters: Vec<String>
 ) -> Result<Vec<VoteResult>> {
     Ok(
