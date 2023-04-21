5b6194702d6551e098f70ee8b143cd7625fa1a6b
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-04-21 17:59:15 +0300

    demo

--- apps/discord-butler/src/main.rs
+++ apps/discord-butler/src/main.rs
@@ -1,4 +1,10 @@
 use log::info;
+use prisma_client_rust::chrono::Utc;
+use serenity::{
+    http::Http,
+    model::{prelude::Embed, webhook::Webhook},
+    utils::Colour,
+};
 mod prisma;
 use std::sync::Arc;
 
@@ -23,4 +29,43 @@ async fn main() {
     info!("discord-butler start");
 
     let _client = Arc::new(PrismaClient::_builder().build().await.unwrap());
+
+    let http = Http::new("");
+    let webhook = Webhook::from_url(&http, "")
+        .await
+        .expect("Replace the webhook with your own");
+
+    let embed_one = Embed::fake(|e| {
+        e.title("A proposal title here with link")
+            .url("https://www.senatelabs.xyz/proposals/active?from=any&end=365&voted=any&proxy=any")
+            .description("with short description with the timestamp thing <t:1683216000:R>")
+            .colour(Colour::from_rgb(255, 0, 0))
+            .thumbnail("https://www.senatelabs.xyz/assets/Icon/NotVotedYet.png")
+            .image("https://www.senatelabs.xyz/assets/Project_Icons/aave_small.png")
+            .timestamp(Utc::now())
+    });
+
+    let embed_two = Embed::fake(|e| {
+        e.title("Another proposal title here with link")
+            .url("https://www.senatelabs.xyz/proposals/past")
+            .description("ends <t:1683216000:R>")
+            .colour(Colour::from_rgb(0, 204, 0))
+            .thumbnail("https://www.senatelabs.xyz/assets/Icon/Voted.png")
+            .image("https://www.senatelabs.xyz/assets/Project_Icons/aave_small.png")
+            .timestamp(Utc::now())
+    });
+
+    let hook = webhook
+        .execute(&http, false, |w| {
+            w.embeds(vec![embed_one])
+                .username("Senate Butler Black")
+                .avatar_url("https://www.senatelabs.xyz/assets/Senate_Logo/64/Black.png")
+        })
+        .await
+        .expect("Could not execute webhook.");
+
+    hook.unwrap()
+        .edit(&http, |m| m.content("new content"))
+        .await
+        .expect("Could not execute webhook.");
 }
