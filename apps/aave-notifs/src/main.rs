86f27d51799c22c6eeba6faff86bd88b7257d6dc
Eugen Patru <eugen.ptr@gmail.com>
2023-05-12 21:10:35 +0900

    WIP

--- apps/aave-notifs/src/main.rs
+++ apps/aave-notifs/src/main.rs
@@ -1,7 +1,8 @@
 #[allow(warnings, unused)]
 pub mod prisma;
 
-use crate::prisma::user;
+use crate::prisma::{user, proposal, notification, dao};
+use prisma_client_rust::chrono::{self, Utc};
 use dotenv::dotenv;
 use std::env;
 
@@ -38,19 +39,75 @@ async fn main() {
         .await
         .unwrap();
 
-    // ================================
-    // Fetch aave proposals
-    // ================================
+    // Fetch proposals to notify about
+    let proposals_ending_soon = db
+        .proposal()
+        .find_many(vec![
+            proposal::timeend::gt(Utc::now().into()),
+            proposal::timeend::lte((Utc::now() + chrono::Duration::hours(12)).into()),
+            proposal::dao::is(vec![
+                dao::name::equals("Aave".to_string()) 
+            ])
+        ])
+        .include(proposal::include!({
+            dao
+        }))
+        .exec()
+        .await
+        .unwrap();
+
+    let proposals_to_notify_about: Vec<_> = proposals_ending_soon
+        .iter()
+        .filter(|&p| p.quorum.as_f64().unwrap() > p.scorestotal.as_f64().unwrap())
+        .collect();
+
+    // Insert notifications to be dispatched
+    for proposal in proposals_to_notify_about.iter() {
+        for user in users_to_be_notified.iter() {
+            db.notification().upsert(
+                notification::userid_proposalid(user.id.to_string(), proposal.id.to_string()),
+                notification::create(
+                    prisma::user::UniqueWhereParam::IdEquals(user.id.clone()),
+                    prisma::proposal::UniqueWhereParam::IdEquals(proposal.id.clone()),
+                    vec![notification::dispatched::set(false)]
+                ),
+                vec![]
+            )
+            .exec()
+            .await
+            .unwrap();
+        }
+    }
+
+    // Fetch all notifications to dispatch
+    let notifications = db
+        .notification()
+        .find_many(vec![
+            notification::dispatched::equals(false),
+            notification::proposal::is(vec![
+                proposal::dao::is(vec![
+                    dao::name::equals("Aave".to_string()),
+                ])
+            ])
+        ])
+        .include(notification::include!({
+            proposal: include {
+                dao
+            }
+            user
+        }))
+        .exec()
+        .await
+        .unwrap();
 
     
-    // ================================
-    // Generate countdown
-    // ================================
+    for n in notifications.iter() {
+        // Send email to n.user.email
+        // send_email().await;
 
-
-    // Send email
-    send_email();
-
+        // If email was successfully sent, set dispatched=true
+    }
+  
     println!("Finished");
 }
 
@@ -66,7 +123,7 @@ async fn send_email() {
         .headers(headers)
         .json(&json!({
             "From": "info@senatelabs.xyz",
-            "To": "infor@senatelabs.xyz",
+            "To": "info@senatelabs.xyz",
             "Subject": "Postmark test",
             "TextBody": "Hello dear Postmark user.",
             "HtmlBody": "<html><body><strong>Hello</strong> dear Postmark user.</body></html>",
