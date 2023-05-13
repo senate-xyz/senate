ebde0c2a7d6dc58e35528bc0653e0fb53ad2517e
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-05-13 12:09:39 +0300

    Update UserEmail.tsx

--- apps/senate/src/app/settings/notifications/components/csr/UserEmail.tsx
+++ apps/senate/src/app/settings/notifications/components/csr/UserEmail.tsx
@@ -67,93 +67,94 @@ const UserEmail = () => {
                     <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-green-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-gray-700" />
                 </label>
             </div>
-
-            <div className='flex flex-row items-center gap-4'>
-                <div className='font-[18px] leading-[23px] text-white'>
-                    Get empty emails
-                </div>
-                <label className='relative inline-flex cursor-pointer items-center bg-gray-400'>
-                    <input
-                        type='checkbox'
-                        checked={getEmptyEmails}
-                        onChange={(e) => {
-                            updateEmptyEmails.mutate({
-                                val: e.target.checked
-                            })
-                        }}
-                        className='peer sr-only'
-                    />
-                    <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-green-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-gray-700" />
-                </label>
-            </div>
-
-            <div className='flex flex-row items-center gap-4'>
-                <div className='font-[18px] leading-[23px] text-white'>
-                    Get quorum alerts
-                </div>
-                <label className='relative inline-flex cursor-pointer items-center bg-gray-400'>
-                    <input
-                        type='checkbox'
-                        checked={getEmailQuorum}
-                        onChange={(e) => {
-                            updateEmailQuorum.mutate({
-                                val: e.target.checked
-                            })
-                        }}
-                        className='peer sr-only'
-                    />
-                    <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-green-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-gray-700" />
-                </label>
-            </div>
-
             {getDailyEmails && (
-                <div className='flex flex-col gap-2'>
-                    <div className='text-[18px] font-light text-white'>
-                        Your Email Address
+                <div className='flex flex-col gap-8'>
+                    <div className='flex flex-row items-center gap-4'>
+                        <div className='font-[18px] leading-[23px] text-white'>
+                            Get empty emails
+                        </div>
+                        <label className='relative inline-flex cursor-pointer items-center bg-gray-400'>
+                            <input
+                                type='checkbox'
+                                checked={getEmptyEmails}
+                                onChange={(e) => {
+                                    updateEmptyEmails.mutate({
+                                        val: e.target.checked
+                                    })
+                                }}
+                                className='peer sr-only'
+                            />
+                            <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-green-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-gray-700" />
+                        </label>
                     </div>
 
-                    <div
-                        className={`flex h-[46px] w-full flex-row items-center`}
-                    >
-                        <input
-                            className={`h-full w-full bg-[#D9D9D9] px-2 text-black focus:outline-none lg:w-[320px] `}
-                            value={currentEmail}
-                            onChange={(e) => {
-                                setCurrentEmail(String(e.target.value))
-                            }}
-                            onKeyDown={(e) => {
-                                if (e.key === 'Enter') onEnter()
-                            }}
-                        />
+                    <div className='flex flex-row items-center gap-4'>
+                        <div className='font-[18px] leading-[23px] text-white'>
+                            Get quorum alerts
+                        </div>
+                        <label className='relative inline-flex cursor-pointer items-center bg-gray-400'>
+                            <input
+                                type='checkbox'
+                                checked={getEmailQuorum}
+                                onChange={(e) => {
+                                    updateEmailQuorum.mutate({
+                                        val: e.target.checked
+                                    })
+                                }}
+                                className='peer sr-only'
+                            />
+                            <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-green-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-gray-700" />
+                        </label>
+                    </div>
+
+                    <div className='flex flex-col gap-2'>
+                        <div className='text-[18px] font-light text-white'>
+                            Your Email Address
+                        </div>
 
                         <div
-                            className={`flex h-full w-[72px] cursor-pointer flex-col justify-center ${
-                                user.data?.email == currentEmail
-                                    ? ' bg-[#ABABAB]'
-                                    : 'bg-white'
-                            } text-center`}
-                            onClick={() => onEnter()}
+                            className={`flex h-[46px] w-full flex-row items-center`}
                         >
-                            Save
-                        </div>
-                    </div>
+                            <input
+                                className={`h-full w-full bg-[#D9D9D9] px-2 text-black focus:outline-none lg:w-[320px] `}
+                                value={currentEmail}
+                                onChange={(e) => {
+                                    setCurrentEmail(String(e.target.value))
+                                }}
+                                onKeyDown={(e) => {
+                                    if (e.key === 'Enter') onEnter()
+                                }}
+                            />
 
-                    {!user.data?.verifiedemail &&
-                        user.data?.challengecode?.length && (
-                            <div className='text-[18px] font-light text-red-400'>
-                                Email not verified!
+                            <div
+                                className={`flex h-full w-[72px] cursor-pointer flex-col justify-center ${
+                                    user.data?.email == currentEmail
+                                        ? ' bg-[#ABABAB]'
+                                        : 'bg-white'
+                                } text-center`}
+                                onClick={() => onEnter()}
+                            >
+                                Save
+                            </div>
+                        </div>
+
+                        {!user.data?.verifiedemail &&
+                            user.data?.challengecode?.length && (
+                                <div className='text-[18px] font-light text-red-400'>
+                                    Email not verified!
+                                </div>
+                            )}
+
+                        {setEmail.error && (
+                            <div className='flex flex-col text-white'>
+                                {JSON.parse(setEmail.error.message).map(
+                                    (err: Error) => (
+                                        <div>{err.message}</div>
+                                    )
+                                )}
                             </div>
                         )}
-
-                    {setEmail.error && (
-                        <div className='flex flex-col text-white'>
-                            {JSON.parse(setEmail.error.message).map(
-                                (err: Error) => (
-                                    <div>{err.message}</div>
-                                )
-                            )}
-                        </div>
-                    )}
+                    </div>
                 </div>
             )}
         </div>
