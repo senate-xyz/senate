c27b27a4bceb7b35b0facfb518cc9f2d42eef2fa
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-05-17 19:43:35 +0300

    wip

---
+++ apps/discord-butler/src/generate/new_proposals.rs
@@ -0,0 +1,84 @@
+use std::sync::Arc;
+
+use crate::prisma::{
+    self,
+    notification,
+    subscription,
+    NotificationType,
+    PrismaClient,
+    ProposalState,
+};
+use anyhow::Result;
+
+pub async fn generate_new_proposal_notifications(client: &Arc<PrismaClient>) {
+    let users = client
+        .user()
+        .find_many(vec![
+            prisma::user::discordnotifications::equals(true),
+            prisma::user::discordwebhook::starts_with("https://".to_string()),
+        ])
+        .exec()
+        .await
+        .unwrap();
+
+    for user in users {
+        let new_proposals = get_new_proposals_for_user(&user.address, client)
+            .await
+            .unwrap();
+
+        client
+            .notification()
+            .create_many(
+                new_proposals
+                    .iter()
+                    .map(|np| {
+                        notification::create_unchecked(
+                            user.clone().id,
+                            np.clone().id,
+                            NotificationType::NewProposalDiscord,
+                            vec![notification::dispatched::set(false)],
+                        )
+                    })
+                    .collect(),
+            )
+            .skip_duplicates()
+            .exec()
+            .await
+            .unwrap();
+    }
+}
+
+prisma::proposal::include!(proposal_with_dao { dao daohandler });
+
+pub async fn get_new_proposals_for_user(
+    username: &String,
+    client: &Arc<PrismaClient>,
+) -> Result<Vec<proposal_with_dao::Data>> {
+    let user = client
+        .user()
+        .find_first(vec![prisma::user::address::equals(username.clone())])
+        .exec()
+        .await
+        .unwrap()
+        .unwrap();
+
+    let subscribed_daos = client
+        .subscription()
+        .find_many(vec![subscription::userid::equals(user.id)])
+        .exec()
+        .await
+        .unwrap();
+
+    let proposals = client
+        .proposal()
+        .find_many(vec![
+            prisma::proposal::daoid::in_vec(subscribed_daos.into_iter().map(|d| d.daoid).collect()),
+            prisma::proposal::state::equals(ProposalState::Active),
+        ])
+        .include(proposal_with_dao::include())
+        .exec()
+        .await
+        .unwrap();
+
+    Ok(proposals)
+}
--- apps/discord-butler/src/main.rs
+++ apps/discord-butler/src/main.rs
@@ -1,20 +1,20 @@
 #![deny(unused_crate_dependencies)]
 
-mod messages;
+mod dispatch;
+mod generate;
 pub mod prisma;
 mod utils {
     pub mod vote;
 }
 
-use log::info;
-use serenity::{http::Http, model::webhook::Webhook};
 use std::{sync::Arc, time::Duration};
 use tokio::time::sleep;
 
 use env_logger::{Builder, Env};
 
 use crate::{
-    messages::{ended::get_past_embeds, ending_soon::get_ending_soon_embeds, new::get_new_embeds},
+    dispatch::new_proposals::dispatch_new_proposal_notifications,
+    generate::new_proposals::generate_new_proposal_notifications,
     prisma::PrismaClient,
 };
 
@@ -33,122 +33,16 @@ fn init_logger() {
 async fn main() {
     init_logger();
 
-    info!("discord-butler start");
-
     let client = Arc::new(PrismaClient::_builder().build().await.unwrap());
 
-    notify_all_users(&client).await;
+    let new_proposals_task = tokio::task::spawn_blocking(move || async move {
+        loop {
+            generate_new_proposal_notifications(&client).await;
+            dispatch_new_proposal_notifications(&client).await;
 
-    info!("discord-butler end");
-}
+            sleep(Duration::from_secs(60)).await;
+        }
+    });
 
-async fn notify_all_users(client: &Arc<PrismaClient>) {
-    info!("discord-butler notify_all_users");
-
-    let users = client
-        .user()
-        .find_many(vec![
-            prisma::user::discordnotifications::equals(true),
-            prisma::user::discordwebhook::starts_with("https://".to_string()),
-        ])
-        .exec()
-        .await
-        .unwrap();
-
-    for user in users {
-        notify_user(user.address, user.discordwebhook, &client).await;
-    }
-}
-
-async fn notify_user(username: String, webhook_url: String, client: &Arc<PrismaClient>) {
-    let http = Http::new("");
-
-    let webhook = Webhook::from_url(&http, webhook_url.as_str())
-        .await
-        .expect("Missing webhook");
-
-    let ending_soon_embeds = get_ending_soon_embeds(&username, client).await.unwrap();
-
-    webhook
-        .execute(&http, false, |w| w.content("**Proposals Ending Soon** The voting on these proposals is going to end in the next 72 hours. You might want to act on them soon.")
-        .username("Senate Butler")
-        .avatar_url("https://www.senatelabs.xyz/assets/Discord/Profile_picture.gif"))
-        .await
-        .expect("Could not execute webhook.");
-
-    sleep(Duration::from_secs(1)).await;
-
-    for chunk in ending_soon_embeds.chunks(5) {
-        webhook
-            .execute(&http, false, |w| {
-                w.embeds(chunk.to_vec())
-                    .username("Senate Butler")
-                    .avatar_url("https://www.senatelabs.xyz/assets/Discord/Profile_picture.gif")
-            })
-            .await
-            .expect("Could not execute webhook.");
-
-        sleep(Duration::from_secs(1)).await;
-    }
-
-    ////
-
-    let new_embeds = get_new_embeds(&username, client).await.unwrap();
-
-    webhook
-        .execute(&http, false, |w| w.content("**New Proposals** These are the proposals that were created in the last 24 hours. You might want to check them out.")
-        .username("Senate Butler")
-       .avatar_url("https://www.senatelabs.xyz/assets/Discord/Profile_picture.gif"))
-        .await
-        .expect("Could not execute webhook.");
-
-    sleep(Duration::from_secs(1)).await;
-
-    for chunk in new_embeds.chunks(5) {
-        webhook
-            .execute(&http, false, |w| {
-                w.embeds(chunk.to_vec())
-                    .username("Senate Butler")
-                    .avatar_url("https://www.senatelabs.xyz/assets/Discord/Profile_picture.gif")
-            })
-            .await
-            .expect("Could not execute webhook.");
-
-        sleep(Duration::from_secs(1)).await;
-    }
-
-    ////
-
-    let past_embeds = get_past_embeds(&username, client).await.unwrap();
-
-    webhook
-        .execute(&http, false, |w| w.content("**Past Proposals** These are the proposals that ended in the last 24 hours. You might want to check them out.")
-        .username("Senate Butler")
-        .avatar_url("https://www.senatelabs.xyz/assets/Discord/Profile_picture.gif"))
-        .await
-        .expect("Could not execute webhook.");
-
-    sleep(Duration::from_secs(1)).await;
-
-    for chunk in past_embeds.chunks(5) {
-        webhook
-            .execute(&http, false, |w| {
-                w.embeds(chunk.to_vec())
-                    .username("Senate Butler")
-                    .avatar_url("https://www.senatelabs.xyz/assets/Discord/Profile_picture.gif")
-            })
-            .await
-            .expect("Could not execute webhook.");
-
-        sleep(Duration::from_secs(1)).await;
-    }
-
-    ////
-
-    webhook
-        .execute(&http, false, |w| w.content("These are the proposals to the DAOs you’re subscribed to on Senate.You can edit your DAO subscriptions and notification settings on https://www.senatelabs.xyz/settings/notifications")
-        .username("Senate Butler")
-        .avatar_url("https://www.senatelabs.xyz/assets/Discord/Profile_picture.gif"))
-        .await
-        .expect("Could not execute webhook.");
+    new_proposals_task.await.unwrap().await;
 }
