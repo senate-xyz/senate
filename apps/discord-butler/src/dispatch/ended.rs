9c42e93e6bbe37a855fca28410a1e149e237bead
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-05-18 13:20:53 +0300

    Update ended.rs

--- apps/discord-butler/src/dispatch/ended.rs
+++ apps/discord-butler/src/dispatch/ended.rs
@@ -43,102 +43,110 @@ pub async fn dispatch_ended_proposal_notifications(client: &Arc<PrismaClient>) {
             ))
             .exec()
             .await
-            .unwrap()
             .unwrap();
 
-        if new_notification.dispatched {
-            let user = client
-                .user()
-                .find_first(vec![user::id::equals(notification.clone().userid)])
-                .exec()
-                .await
-                .unwrap()
-                .unwrap();
+        match new_notification {
+            Some(new_notification) => {
+                if new_notification.dispatched {
+                    let user = client
+                        .user()
+                        .find_first(vec![user::id::equals(notification.clone().userid)])
+                        .exec()
+                        .await
+                        .unwrap()
+                        .unwrap();
 
-            let proposal = client
-                .proposal()
-                .find_first(vec![proposal::id::equals(notification.clone().proposalid)])
-                .include(proposal_with_dao::include())
-                .exec()
-                .await
-                .unwrap()
-                .unwrap();
+                    let proposal = client
+                        .proposal()
+                        .find_first(vec![proposal::id::equals(notification.clone().proposalid)])
+                        .include(proposal_with_dao::include())
+                        .exec()
+                        .await
+                        .unwrap()
+                        .unwrap();
 
-            let initial_message_id: u64 =
-                new_notification.discordmessageid.unwrap().parse().unwrap();
+                    let initial_message_id: u64 =
+                        new_notification.discordmessageid.unwrap().parse().unwrap();
 
-            let http = Http::new("");
+                    let http = Http::new("");
 
-            let webhook = Webhook::from_url(&http, user.discordwebhook.as_str())
-                .await
-                .expect("Missing webhook");
+                    let webhook = Webhook::from_url(&http, user.discordwebhook.as_str())
+                        .await
+                        .expect("Missing webhook");
 
-            webhook
-                .edit_message(&http, MessageId::from(initial_message_id), |w| {
-                    w.embeds(vec![Embed::fake(|e| {
-                        e.title(proposal.name)
-                            .description(format!(
-                                "**{}** {} proposal ended with results {}",
+                    webhook
+                        .edit_message(&http, MessageId::from(initial_message_id), |w| {
+                            w.embeds(vec![Embed::fake(|e| {
+                                e.title(proposal.name)
+                                    .description(format!(
+                                        "**{}** {} proposal ended with results {}",
+                                        proposal.dao.name,
+                                        if proposal.daohandler.r#type == DaoHandlerType::Snapshot {
+                                            "off-chain"
+                                        } else {
+                                            "on-chain"
+                                        },
+                                        proposal.scores
+                                    ))
+                                    .url(proposal.url)
+                                    .color(Colour::DARK_GREEN)
+                                    .thumbnail(format!(
+                                        "https://www.senatelabs.xyz/{}_medium.png",
+                                        proposal.dao.picture
+                                    ))
+                                    .image("https://placehold.co/2000x1/png")
+                            })])
+                        })
+                        .await
+                        .unwrap();
+
+                    let message = webhook
+                        .execute(&http, true, |w| {
+                            w.content(format!(
+                                "🗳️ **{}** {} proposal {} **just ended.** ☑️",
                                 proposal.dao.name,
                                 if proposal.daohandler.r#type == DaoHandlerType::Snapshot {
                                     "off-chain"
                                 } else {
                                     "on-chain"
                                 },
-                                proposal.scores
+                                new_notification.discordmessagelink.unwrap(),
                             ))
-                            .url(proposal.url)
-                            .color(Colour::DARK_GREEN)
-                            .thumbnail(format!(
-                                "https://www.senatelabs.xyz/{}_medium.png",
-                                proposal.dao.picture
-                            ))
-                            .image("https://placehold.co/2000x1/png")
-                    })])
-                })
-                .await
-                .unwrap();
-
-            let message = webhook
-                .execute(&http, true, |w| {
-                    w.content(format!(
-                        "🗳️ **{}** {} proposal {} **just ended.** ☑️",
-                        proposal.dao.name,
-                        if proposal.daohandler.r#type == DaoHandlerType::Snapshot {
-                            "off-chain"
-                        } else {
-                            "on-chain"
-                        },
-                        new_notification.discordmessagelink.unwrap(),
-                    ))
-                    .username("Senate Butler")
-                    .avatar_url("https://www.senatelabs.xyz/assets/Discord/Profile_picture.gif")
-                })
-                .await
-                .expect("Could not execute webhook.");
-
-            match message {
-                Some(msg) => {
-                    client
-                        .notification()
-                        .update(
-                            notification::userid_proposalid_type(
-                                notification.clone().userid,
-                                notification.clone().proposalid,
-                                notification.clone().r#type,
-                            ),
-                            vec![
-                                notification::dispatched::set(true),
-                                notification::discordmessagelink::set(msg.link().into()),
-                                notification::discordmessageid::set(msg.id.to_string().into()),
-                            ],
-                        )
-                        .exec()
+                            .username("Senate Butler")
+                            .avatar_url(
+                                "https://www.senatelabs.xyz/assets/Discord/Profile_picture.gif",
+                            )
+                        })
                         .await
-                        .unwrap();
+                        .expect("Could not execute webhook.");
+
+                    match message {
+                        Some(msg) => {
+                            client
+                                .notification()
+                                .update(
+                                    notification::userid_proposalid_type(
+                                        notification.clone().userid,
+                                        notification.clone().proposalid,
+                                        notification.clone().r#type,
+                                    ),
+                                    vec![
+                                        notification::dispatched::set(true),
+                                        notification::discordmessagelink::set(msg.link().into()),
+                                        notification::discordmessageid::set(
+                                            msg.id.to_string().into(),
+                                        ),
+                                    ],
+                                )
+                                .exec()
+                                .await
+                                .unwrap();
+                        }
+                        None => {}
+                    }
                 }
-                None => {}
             }
+            None => {}
         }
 
         sleep(Duration::from_millis(100)).await;
