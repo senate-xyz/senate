199a5c0fe9fee2836724e29460132cb72878ee00
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-05-24 08:52:15 +0300

    updated email field

--- apps/senate/src/app/settings/notifications/components/csr/UserEmail.tsx
+++ apps/senate/src/app/settings/notifications/components/csr/UserEmail.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation'
 import { useAccount } from 'wagmi'
 
 const UserEmail = () => {
+    const [edit, setEdit] = useState(false)
     const [currentEmail, setCurrentEmail] = useState('')
     const [getDailyEmails, setDailyEmails] = useState(false)
     const [getEmptyEmails, setEmptyEmails] = useState(false)
@@ -45,12 +46,17 @@ const UserEmail = () => {
         trpc.accountSettings.updateQuorumEmails.useMutation()
 
     const onEnter = () => {
-        setEmail.mutate({ email: currentEmail })
+        setEmail.mutate(
+            { email: currentEmail },
+            {
+                onSuccess: () => {
+                    setEdit(false)
+                }
+            }
+        )
     }
 
     if (!user.data) return <></>
-
-    console.log(user.data.email + currentEmail)
 
     return (
         <div className='flex flex-col'>
@@ -76,7 +82,7 @@ const UserEmail = () => {
                         }}
                         className='peer sr-only'
                     />
-                    <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-green-400 peer-checked:after:translate-x-full peer-checked:hover:bg-green-300" />
+                    <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-[#5EF413] peer-checked:after:translate-x-full peer-checked:hover:bg-[#7EF642]" />
                 </label>
             </div>
             {getDailyEmails && (
@@ -86,35 +92,53 @@ const UserEmail = () => {
                             Your Email Address
                         </div>
 
-                        <div
-                            className={`flex h-[46px] max-w-[382px] flex-row items-center`}
-                        >
-                            <input
-                                className={`h-full w-full bg-[#D9D9D9] px-2 text-black focus:outline-none lg:w-[320px] `}
-                                value={currentEmail}
-                                onChange={(e) => {
-                                    setCurrentEmail(String(e.target.value))
-                                }}
-                                onKeyDown={(e) => {
-                                    if (e.key === 'Enter') onEnter()
-                                }}
-                            />
-
+                        {edit || !user.data.email ? (
                             <div
-                                className={`flex h-full w-[72px] cursor-pointer flex-col justify-center ${
-                                    user.data.email?.includes('@')
-                                        ? user.data.email == currentEmail
-                                            ? 'bg-[#ABABAB] hover:bg-[#999999]'
-                                            : 'bg-white hover:bg-[#e5e5e5]'
-                                        : currentEmail.length
-                                        ? 'bg-white hover:bg-[#e5e5e5]'
-                                        : 'bg-[#ABABAB] hover:bg-[#999999]'
-                                } text-center`}
-                                onClick={() => onEnter()}
+                                className={`flex h-[46px] max-w-[382px] flex-row items-center`}
                             >
-                                Save
+                                <input
+                                    className={`h-full w-full bg-[#D9D9D9] px-2 text-black focus:outline-none lg:w-[320px] `}
+                                    value={currentEmail}
+                                    onChange={(e) => {
+                                        setCurrentEmail(String(e.target.value))
+                                    }}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') onEnter()
+                                    }}
+                                />
+
+                                <div
+                                    className={`flex h-full w-[72px] cursor-pointer flex-col justify-center ${
+                                        user.data.email?.includes('@')
+                                            ? user.data.email == currentEmail
+                                                ? 'bg-[#ABABAB] hover:bg-[#999999]'
+                                                : 'bg-white hover:bg-[#e5e5e5]'
+                                            : currentEmail.length
+                                            ? 'bg-white hover:bg-[#e5e5e5]'
+                                            : 'bg-[#ABABAB] hover:bg-[#999999]'
+                                    } text-center`}
+                                    onClick={() => onEnter()}
+                                >
+                                    Save
+                                </div>
                             </div>
-                        </div>
+                        ) : (
+                            <div
+                                className={`flex h-[46px] max-w-[382px] flex-row items-center justify-between`}
+                            >
+                                <div className='text-[18px] font-light text-white'>
+                                    {currentEmail}
+                                </div>
+                                <div
+                                    className='cursor-pointer text-[18px] font-light text-white underline'
+                                    onClick={() => {
+                                        setEdit(true)
+                                    }}
+                                >
+                                    Change Email
+                                </div>
+                            </div>
+                        )}
 
                         {getEmailUpdated && (
                             <div className='text-[18px] font-light text-green-400'>
@@ -122,7 +146,7 @@ const UserEmail = () => {
                             </div>
                         )}
 
-                        {!user.data.verifiedemail && (
+                        {!user.data.verifiedemail && user.data.email && (
                             <div className='text-[18px] font-light text-red-400'>
                                 Email not verified!
                             </div>
@@ -154,7 +178,7 @@ const UserEmail = () => {
                                 }}
                                 className='peer sr-only'
                             />
-                            <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-green-400 peer-checked:after:translate-x-full peer-checked:hover:bg-green-300" />
+                            <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-[#5EF413] peer-checked:after:translate-x-full peer-checked:hover:bg-[#7EF642]" />
                         </label>
                     </div>
 
@@ -173,7 +197,7 @@ const UserEmail = () => {
                                 }}
                                 className='peer sr-only'
                             />
-                            <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-green-400 peer-checked:after:translate-x-full peer-checked:hover:bg-green-300" />
+                            <div className="peer h-6 w-11 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5  after:bg-black after:transition-all after:content-[''] peer-checked:bg-[#5EF413] peer-checked:after:translate-x-full peer-checked:hover:bg-[#7EF642]" />
                         </label>
                     </div>
                 </div>
