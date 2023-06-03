1dd113b0da35ca90c520a304b8ebd9f281ac056f
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-06-03 16:57:25 +0300

    wip

--- apps/email-secretary/src/bulletin/bulletin_emails.rs
+++ apps/email-secretary/src/bulletin/bulletin_emails.rs
@@ -1,4 +1,4 @@
-use std::{env, sync::Arc};
+use std::{cmp::Ordering, env, sync::Arc};
 
 use anyhow::Result;
 use chrono::{Duration, Utc};
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 use crate::{
-    prisma::{self, proposal, user, DaoHandlerType, MagicUserState},
+    prisma::{self, proposal, user, DaoHandlerType, MagicUserState, ProposalState},
     utils::{countdown::countdown_gif, vote::get_vote},
 };
 
@@ -38,6 +38,7 @@ struct BulletinData {
 struct EndingSoonProposals {
     daoLogoUrl: String,
     chainLogoUrl: String,
+    url: String,
     proposalName: String,
     countdownUrl: String,
     countdownString: String,
@@ -50,6 +51,7 @@ struct EndingSoonProposals {
 struct NewProposals {
     daoLogoUrl: String,
     chainLogoUrl: String,
+    url: String,
     proposalName: String,
     countdownUrl: String,
     countdownString: String,
@@ -62,11 +64,15 @@ struct NewProposals {
 struct EndedProposals {
     daoLogoUrl: String,
     chainLogoUrl: String,
+    url: String,
+    proposalName: String,
     hiddenResult: bool,
-    result: NormalResult,
+    result: Option<NormalResult>,
     noqorum: bool,
-    makerResult: MakerResult,
+    makerResult: Option<MakerResult>,
     countdownString: String,
+    voteStatusIconUrl: String,
+    voteStatus: String,
 }
 
 #[allow(non_snake_case)]
@@ -74,8 +80,6 @@ struct EndedProposals {
 struct NormalResult {
     choiceName: String,
     choicePercentage: i64,
-    voteStatusIconUrl: String,
-    voteStatus: String,
 }
 
 #[allow(non_snake_case)]
@@ -142,15 +146,15 @@ async fn send_bulletin(
     //     .unwrap();
     //let _success = res.status().is_success();
 
-    println!(
-        "{:?}",
-        &EmailBody {
-            To: user_email,
-            From: "info@senatelabs.xyz".to_string(),
-            TemplateAlias: bulletin_template.to_string(),
-            TemplateModel: user_data.clone(),
-        }
-    );
+    // println!(
+    //     "{:?}",
+    //     &EmailBody {
+    //         To: user_email,
+    //         From: "info@senatelabs.xyz".to_string(),
+    //         TemplateAlias: bulletin_template.to_string(),
+    //         TemplateModel: user_data.clone(),
+    //     }
+    // );
 
     println!("{}", serde_json::to_string(&user_data).unwrap());
 
@@ -180,7 +184,7 @@ async fn get_ending_soon_proposals(
     let proposals = db
         .proposal()
         .find_many(vec![
-            // proposal::timeend::lte((Utc::now() + Duration::from(Duration::days(3))).into()),
+            proposal::timeend::lte((Utc::now() + Duration::from(Duration::days(3))).into()),
             proposal::timeend::gt(Utc::now().into()),
             proposal::daoid::in_vec(
                 user.subscriptions
@@ -189,6 +193,7 @@ async fn get_ending_soon_proposals(
                     .map(|s| s.daoid)
                     .collect(),
             ),
+            proposal::state::not(ProposalState::Canceled),
         ])
         .order_by(proposal::timeend::order(Direction::Asc))
         .include(proposal_with_dao::include())
@@ -198,8 +203,23 @@ async fn get_ending_soon_proposals(
 
     let ending_proposals = futures::future::join_all(proposals.iter().map(|p| async {
         let countdown_url = countdown_gif(p.timeend.into()).await.unwrap();
-        println!("{}", countdown_url);
         let voted = get_vote(user.clone().id, p.clone().id, db).await.unwrap();
+
+        let shortner_url = match env::var_os("NEXT_PUBLIC_URL_SHORTNER") {
+            Some(v) => v.into_string().unwrap(),
+            None => panic!("$NEXT_PUBLIC_URL_SHORTNER is not set"),
+        };
+        let short_url = format!(
+            "{}{}",
+            shortner_url,
+            p.id.chars()
+                .rev()
+                .take(7)
+                .collect::<Vec<char>>()
+                .into_iter()
+                .rev()
+                .collect::<String>()
+        );
         EndingSoonProposals {
             daoLogoUrl: format!(
                 "{}{}{}",
@@ -214,6 +234,7 @@ async fn get_ending_soon_proposals(
                     "/assets/Icon/on-chain.png"
                 }
             ),
+            url: short_url,
             proposalName: p.clone().name,
             countdownUrl: countdown_url,
             countdownString: p.timeend.format("on %b %e, %Y at %H:%M UTC").to_string(),
@@ -235,15 +256,195 @@ async fn get_ending_soon_proposals(
 }
 
 async fn get_new_proposals(
-    _user: user_with_voters_and_subscriptions::Data,
-    _db: &Arc<prisma::PrismaClient>,
+    user: user_with_voters_and_subscriptions::Data,
+    db: &Arc<prisma::PrismaClient>,
 ) -> Result<Vec<NewProposals>> {
-    Ok(vec![])
+    let proposals = db
+        .proposal()
+        .find_many(vec![
+            proposal::timecreated::gte((Utc::now() - Duration::from(Duration::days(1))).into()),
+            proposal::daoid::in_vec(
+                user.subscriptions
+                    .clone()
+                    .into_iter()
+                    .map(|s| s.daoid)
+                    .collect(),
+            ),
+            proposal::state::not(ProposalState::Canceled),
+        ])
+        .order_by(proposal::timeend::order(Direction::Asc))
+        .include(proposal_with_dao::include())
+        .exec()
+        .await
+        .unwrap();
+
+    let new_proposals = futures::future::join_all(proposals.iter().map(|p| async {
+        let countdown_url = countdown_gif(p.timeend.into()).await.unwrap();
+        let voted = get_vote(user.clone().id, p.clone().id, db).await.unwrap();
+        let shortner_url = match env::var_os("NEXT_PUBLIC_URL_SHORTNER") {
+            Some(v) => v.into_string().unwrap(),
+            None => panic!("$NEXT_PUBLIC_URL_SHORTNER is not set"),
+        };
+        let short_url = format!(
+            "{}{}",
+            shortner_url,
+            p.id.chars()
+                .rev()
+                .take(7)
+                .collect::<Vec<char>>()
+                .into_iter()
+                .rev()
+                .collect::<String>()
+        );
+
+        NewProposals {
+            daoLogoUrl: format!(
+                "{}{}{}",
+                "https://senatelabs.xyz", p.dao.picture, "_medium.png"
+            ),
+            chainLogoUrl: format!(
+                "{}{}",
+                "https://senatelabs.xyz",
+                if p.daohandler.r#type == DaoHandlerType::Snapshot {
+                    "/assets/Icon/off-chain.png"
+                } else {
+                    "/assets/Icon/on-chain.png"
+                }
+            ),
+            url: short_url,
+            proposalName: p.clone().name,
+            countdownUrl: countdown_url,
+            countdownString: p.timeend.format("on %b %e, %Y at %H:%M UTC").to_string(),
+            voteStatusIconUrl: if voted {
+                "https://senatelabs.xyz/assets/Emails/voted.png".to_string()
+            } else {
+                "https://senatelabs.xyz/assets/Emails/not-voted-yet.png".to_string()
+            },
+            voteStatus: if voted {
+                "Voted".to_string()
+            } else {
+                "Not voted yet".to_string()
+            },
+        }
+    }))
+    .await;
+
+    Ok(new_proposals)
 }
 
 async fn get_ended_proposals(
-    _user: user_with_voters_and_subscriptions::Data,
-    _db: &Arc<prisma::PrismaClient>,
+    user: user_with_voters_and_subscriptions::Data,
+    db: &Arc<prisma::PrismaClient>,
 ) -> Result<Vec<EndedProposals>> {
-    Ok(vec![])
+    let proposals = db
+        .proposal()
+        .find_many(vec![
+            proposal::timeend::lte(Utc::now().into()),
+            proposal::timeend::gt((Utc::now() - Duration::from(Duration::days(10))).into()),
+            proposal::daoid::in_vec(
+                user.subscriptions
+                    .clone()
+                    .into_iter()
+                    .map(|s| s.daoid)
+                    .collect(),
+            ),
+            proposal::state::not(ProposalState::Canceled),
+        ])
+        .order_by(proposal::timeend::order(Direction::Asc))
+        .include(proposal_with_dao::include())
+        .exec()
+        .await
+        .unwrap();
+
+    let ended_proposals = futures::future::join_all(proposals.iter().map(|p| async {
+        let voted = get_vote(user.clone().id, p.clone().id, db).await.unwrap();
+        let shortner_url = match env::var_os("NEXT_PUBLIC_URL_SHORTNER") {
+            Some(v) => v.into_string().unwrap(),
+            None => panic!("$NEXT_PUBLIC_URL_SHORTNER is not set"),
+        };
+        let short_url = format!(
+            "{}{}",
+            shortner_url,
+            p.id.chars()
+                .rev()
+                .take(7)
+                .collect::<Vec<char>>()
+                .into_iter()
+                .rev()
+                .collect::<String>()
+        );
+
+        let (result_index, max_score) = p
+            .scores
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|score| score.as_f64().unwrap())
+            .enumerate()
+            .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap_or(Ordering::Equal))
+            .unwrap_or((100, 0.0));
+
+        EndedProposals {
+            daoLogoUrl: format!(
+                "{}{}{}",
+                "https://senatelabs.xyz", p.dao.picture, "_medium.png"
+            ),
+            chainLogoUrl: format!(
+                "{}{}",
+                "https://senatelabs.xyz",
+                if p.daohandler.r#type == DaoHandlerType::Snapshot {
+                    "/assets/Icon/off-chain.png"
+                } else {
+                    "/assets/Icon/on-chain.png"
+                }
+            ),
+            url: short_url,
+            proposalName: p.clone().name,
+            countdownString: p.timeend.format("on %b %e, %Y at %H:%M UTC").to_string(),
+            voteStatusIconUrl: if voted {
+                "https://senatelabs.xyz/assets/Emails/voted.png".to_string()
+            } else {
+                "https://senatelabs.xyz/assets/Emails/did-not-vote.png".to_string()
+            },
+            voteStatus: if voted {
+                "Voted".to_string()
+            } else {
+                "Did not vote".to_string()
+            },
+            hiddenResult: if p.state == ProposalState::Hidden {
+                true
+            } else {
+                false
+            },
+            result: if p.scorestotal.as_f64() > p.quorum.as_f64()
+                && p.state != ProposalState::Hidden
+            {
+                Some(NormalResult {
+                    choiceName: p.choices.as_array().unwrap()[result_index].to_string(),
+                    choicePercentage: (max_score / p.scorestotal.as_f64().unwrap() * 100.0).round()
+                        as i64,
+                })
+            } else {
+                None
+            },
+            noqorum: if p.scorestotal.as_f64() < p.quorum.as_f64()
+                && p.state != ProposalState::Hidden
+            {
+                true
+            } else {
+                false
+            },
+            makerResult: if p.dao.name == "MakerDAO" {
+                Some(MakerResult {
+                    choiceName: "Yes".to_string(),
+                    mkrAmount: 1,
+                })
+            } else {
+                None
+            },
+        }
+    }))
+    .await;
+
+    Ok(ended_proposals)
 }
