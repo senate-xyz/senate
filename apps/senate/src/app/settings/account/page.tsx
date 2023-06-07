2abc13a2a4985ecd894504a67b292b92f22fc6c3
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-06-07 21:32:16 +0300

    cleanup

--- apps/senate/src/app/settings/account/page.tsx
+++ apps/senate/src/app/settings/account/page.tsx
@@ -1,29 +1,17 @@
 'use client'
 
 import { useAccountModal } from '@rainbow-me/rainbowkit'
-import { signOut, useSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
 import { useAccount } from 'wagmi'
 import NotConnected from './components/csr/NotConnected'
 import UserAddress from './components/csr/UserAddress'
 import Link from 'next/link'
-import { trpc } from '../../../server/trpcClient'
+import Testing from './testing'
 
 export default function Home() {
     const account = useAccount()
     const session = useSession()
     const { openAccountModal } = useAccountModal()
-
-    const featureFlags = trpc.public.featureFlags.useQuery()
-
-    const deleteUser = trpc.accountSettings.deleteUser.useMutation()
-    const randomQuorumAlert = trpc.testing.randomQuorumAlert.useMutation()
-    const lastQuorumAlert = trpc.testing.lastQuorumAlert.useMutation()
-    const lastAaveQuorumAlert = trpc.testing.lastAaveQuorumAlert.useMutation()
-    const lastUniswapQuorumAlert =
-        trpc.testing.lastUniswapQuorumAlert.useMutation()
-    const deleteDispatchedNotifications =
-        trpc.testing.deleteDispatchedNotifications.useMutation()
-    const sendBulletin = trpc.testing.sendBulletin.useMutation()
 
     return (
         <div className='flex min-h-screen flex-col gap-12'>
@@ -82,90 +70,7 @@ export default function Home() {
                     </div>
                 )}
 
-                {featureFlags.data?.includes('testing_stuff') && (
-                    <div className='flex max-w-[400px] flex-col gap-2 border border-red-400 p-2'>
-                        <div className='font-bold text-white'>
-                            Testing stuff
-                        </div>
-                        <div /> <div />
-                        <div
-                            className='w-max cursor-pointer bg-red-400 p-2 text-white'
-                            onClick={() => {
-                                deleteUser.mutate(void 0, {
-                                    onSuccess: () => {
-                                        signOut()
-                                    }
-                                })
-                            }}
-                        >
-                            Delete my own user!
-                        </div>
-                        <div /> <div />
-                        <div className='text-white'>
-                            Can take up to one minute
-                        </div>
-                        <div
-                            className='w-max cursor-pointer bg-red-400 p-2 text-white'
-                            onClick={() => {
-                                randomQuorumAlert.mutate()
-                            }}
-                        >
-                            Send random quorum alert{' '}
-                        </div>
-                        <div
-                            className='w-max cursor-pointer bg-red-400 p-2 text-white'
-                            onClick={() => {
-                                lastQuorumAlert.mutate()
-                            }}
-                        >
-                            Send last proposal quorum alert{' '}
-                        </div>
-                        <div
-                            className='w-max cursor-pointer bg-red-400 p-2 text-white'
-                            onClick={() => {
-                                lastAaveQuorumAlert.mutate()
-                            }}
-                        >
-                            Send last Aave proposal quorum alert
-                        </div>
-                        <div
-                            className='w-max cursor-pointer bg-red-400 p-2 text-white'
-                            onClick={() => {
-                                lastUniswapQuorumAlert.mutate()
-                            }}
-                        >
-                            Send last Uniswap proposal quorum alert
-                        </div>
-                        <div className='text-white'>
-                            Because our backend service works on repeat every
-                            minute and it must not repeat sending the same
-                            notification every time, it saves "already sent"
-                            notifications. Because of this, you can send one
-                            notification only once.
-                        </div>
-                        <div className='text-white'>
-                            You can delete all your saved, dispatched
-                            notification with this button.
-                        </div>
-                        <div
-                            className='w-max cursor-pointer bg-red-400 p-2 text-white'
-                            onClick={() => {
-                                deleteDispatchedNotifications.mutate()
-                            }}
-                        >
-                            Delete dispatched notifications
-                        </div>
-                        <div /> <div />
-                        <div
-                            className='w-max cursor-pointer bg-red-400 p-2 text-white'
-                            onClick={() => {
-                                sendBulletin.mutate()
-                            }}
-                        >
-                            Send bulletin
-                        </div>
-                    </div>
-                )}
+                <Testing />
             </div>
         </div>
     )
