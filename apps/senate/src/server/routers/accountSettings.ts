ad09b882551645d35c52a674526f2b236f9496dc
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-06-26 14:09:28 +0300

    disable geo ip for server side events

--- apps/senate/src/server/routers/accountSettings.ts
+++ apps/senate/src/server/routers/accountSettings.ts
@@ -6,6 +6,7 @@ import { PostHog } from "posthog-node";
 
 const posthog = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
   host: `${process.env.NEXT_PUBLIC_WEB_URL ?? ""}/ingest`,
+  disableGeoip: true,
 });
 
 export const accountSettingsRouter = router({
