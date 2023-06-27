ea46ac5d9c1d081be3173d16a5301f41286cd64d
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-06-27 15:32:44 +0300

    Update main.rs

--- apps/email-secretary/src/main.rs
+++ apps/email-secretary/src/main.rs
@@ -50,8 +50,7 @@ async fn main() {
 
     let mut scheduler = AsyncScheduler::with_tz(chrono::Utc);
     scheduler
-        .every(1_u32.day())
-        .at("12:25 pm")
+        .every(10_u32.minute())
         .run(move || send_bulletin_emails(client_for_bulletin.clone()));
 
     tokio::spawn(async move {
