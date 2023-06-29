af035dcd2b9c2cf8bcf9abefd473e38ba3428087
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-06-29 18:47:31 +0300

    past mobile

--- apps/senate/src/app/proposals/past/components/Item.tsx
+++ apps/senate/src/app/proposals/past/components/Item.tsx
@@ -356,9 +356,152 @@ export default function Item(props: {
 
           <div className="flex w-full flex-row items-end justify-between">
             <div className="flex flex-col justify-end">
-              <div className="text-start text-[21px] font-semibold leading-[26px]">
-                {dayjs(props.proposal.timeEnd).fromNow()}
-              </div>
+              {props.proposal.daoHandlerType == "MAKER_EXECUTIVE" && (
+                <div>
+                  <div className="text-[21px] leading-[26px] text-white">
+                    {(props.proposal.state == "EXECUTED" ||
+                      props.proposal.state == "QUEUED") && (
+                      <div className="flex flex-row gap-2">
+                        <div className="h-[24px] w-[24px] items-center justify-center bg-[#D9D9D9]">
+                          <Image
+                            loading="eager"
+                            priority={true}
+                            width={22}
+                            height={22}
+                            src={"/assets/Icon/Check.svg"}
+                            alt="off-chain"
+                          />
+                        </div>
+
+                        <div>Passed</div>
+                      </div>
+                    )}
+                    {props.proposal.state == "EXPIRED" && (
+                      <div className="flex flex-row gap-2">
+                        <div className="h-[24px] w-[24px] items-center justify-center bg-[#D9D9D9]">
+                          <Image
+                            loading="eager"
+                            priority={true}
+                            width={22}
+                            height={22}
+                            src={"/assets/Icon/NoCheck.svg"}
+                            alt="off-chain"
+                          />
+                        </div>
+
+                        <div>Did not pass</div>
+                      </div>
+                    )}
+                  </div>
+                  <div className="text-[18px] leading-[26px] text-white">
+                    with{" "}
+                    {(props.proposal.scoresTotal / 1000000000000000000).toFixed(
+                      2
+                    )}{" "}
+                    MKR
+                  </div>
+                </div>
+              )}
+              {props.proposal.daoHandlerType != "MAKER_EXECUTIVE" &&
+                props.proposal.state != "HIDDEN" &&
+                props.proposal.highestScoreChoice != "undefined" && (
+                  <div>
+                    {props.proposal.passedQuorum ? (
+                      <div>
+                        <div className="flex flex-row gap-2">
+                          <div className="flex h-[24px] w-[24px] items-center justify-center bg-[#D9D9D9]">
+                            <Image
+                              loading="eager"
+                              priority={true}
+                              width={22}
+                              height={22}
+                              src={"/assets/Icon/Check.svg"}
+                              alt="off-chain"
+                            />
+                          </div>
+                          <div className="truncate text-[21px] leading-[26px] text-white">
+                            {props.proposal.highestScoreChoice}
+                          </div>
+                        </div>
+                        <div className="mt-1 bg-[#262626]">
+                          <div
+                            style={{
+                              width: `${(
+                                (props.proposal.highestScore /
+                                  props.proposal.scoresTotal) *
+                                100
+                              ).toFixed(0)}%`,
+                            }}
+                            className={`h-full bg-[#EDEDED]`}
+                          >
+                            <div className="px-2 text-black">
+                              {(
+                                (props.proposal.highestScore /
+                                  props.proposal.scoresTotal) *
+                                100
+                              ).toFixed(2)}
+                              %
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="flex flex-row gap-2">
+                        <div className="flex h-[24px] w-[24px] items-center justify-center bg-[#D9D9D9]">
+                          <Image
+                            loading="eager"
+                            priority={true}
+                            width={22}
+                            height={22}
+                            src={"/assets/Icon/NoCheck.svg"}
+                            alt="off-chain"
+                          />
+                        </div>
+                        <div className="text-[21px] leading-[26px] text-white">
+                          No Quorum
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                )}
+
+              {props.proposal.state == "HIDDEN" && (
+                <div>
+                  <div className="flex flex-row gap-2">
+                    <div className="flex h-[24px] w-[24px] items-center justify-center bg-[#D9D9D9]">
+                      <Image
+                        loading="eager"
+                        priority={true}
+                        width={22}
+                        height={22}
+                        src={"/assets/Icon/Hidden.svg"}
+                        alt="off-chain"
+                      />
+                    </div>
+
+                    <div className="text-[21px] leading-[26px] text-white">
+                      Hidden Results
+                    </div>
+                  </div>
+                  <div className="mt-1 bg-[#262626]">
+                    <div
+                      style={{
+                        width: "100%",
+                      }}
+                      className={`h-full bg-[#EDEDED]`}
+                    >
+                      <div className="px-2 text-black">??</div>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              {props.proposal.daoHandlerType != "MAKER_EXECUTIVE" &&
+                props.proposal.highestScoreChoice == "undefined" && (
+                  <div className="text-[17px] leading-[26px] text-white">
+                    Unable to fetch results data
+                  </div>
+                )}
               <div className="text-[12px] font-normal leading-[19px]">
                 {`on ${new Date(props.proposal.timeEnd).toLocaleDateString(
                   "en-US",
