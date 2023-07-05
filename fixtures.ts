88ff09745bfac3f3b18d0bccb81e43df649af0b1
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-07-05 15:26:25 +0300

    test

--- fixtures.ts
+++ fixtures.ts
@@ -6,42 +6,44 @@ export const test = base.extend<{
   context: BrowserContext;
 }>({
   context: async ({}, use) => {
+    // required for synpress
+    //@ts-ignore
+    global.expect = expect;
     // download metamask
     const metamaskPath = await prepareMetamask(
       process.env.METAMASK_VERSION || "10.25.0"
     );
     // prepare browser args
     const browserArgs = [
-      // `--disable-extensions-except=${metamaskPath}`,
-      // `--load-extension=${metamaskPath}`,
+      `--disable-extensions-except=${metamaskPath}`,
+      `--load-extension=${metamaskPath}`,
       "--remote-debugging-port=9222",
-      "--disable-dev-shm-usage",
-      "--ipc=host",
-      "--single-process",
-      "--disable-gpu",
     ];
-
-    console.log(browserArgs);
+    if (process.env.CI) {
+      browserArgs.push("--disable-gpu");
+    }
+    if (process.env.HEADLESS_MODE) {
+      browserArgs.push("--headless=new");
+    }
     // launch browser
-    const context = await chromium.launchPersistentContext(
-      "/tmp/test-user-data",
-      {
-        headless: false,
-        args: browserArgs,
-      }
-    );
+    const context = await chromium.launchPersistentContext("", {
+      headless: false,
+      args: browserArgs,
+    });
+    context.setDefaultTimeout(30000);
     // wait for metamask
-    await context.pages()[0].waitForTimeout(process.env.CI ? 10000 : 2000);
+    await context.pages()[0].waitForTimeout(3000);
     // setup metamask
-    // await initialSetup(chromium, {
-    //   secretWordsOrPrivateKey:
-    //     "test test test test test test test test test test test junk",
-    //   network: "mainnet",
-    //   password: "Tester@1234",
-    //   enableAdvancedSettings: true,
-    //   enableExperimentalSettings: true,
-    // });
+    await initialSetup(chromium, {
+      secretWordsOrPrivateKey:
+        "test test test test test test test test test test test junk",
+      network: "mainnet",
+      password: "Tester@1234",
+      enableAdvancedSettings: true,
+      enableExperimentalSettings: true,
+    });
     await use(context);
     await context.close();
   },
 });
+export const expect = test.expect;
