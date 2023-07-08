82d28d1f8b647e29f3163a35945e3d922eff5a56
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-07-08 22:05:06 +0300

    ci: not needed

--- prepareci.sh
+++ prepareci.sh
@@ -3,15 +3,12 @@
 if [ "${CIRCLE_BRANCH}" == "dev" ]
 then
   export DATABASE_URL=${DEVELOPMENT_DB}
-  npm run build
 elif [ "${CIRCLE_BRANCH}" == "staging" ]
 then
   export DATABASE_URL=${STAGING_DB}
-  npm run build
 elif [ "${CIRCLE_BRANCH}" == "main" ]
 then
   export DATABASE_URL=${PRODUCTION_DB}
-  npm run build
 fi
 
 echo "DATABASE_URL set for : ${CIRCLE_BRANCH}"
