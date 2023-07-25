02f1558c4e5a0c615ffc0cb3c4ef15b8eba2d1db
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-07-25 20:16:16 +0300

    fix: cors options

--- apps/senate/src/pages/api/discourse/uniswap-magic-user.ts
+++ apps/senate/src/pages/api/discourse/uniswap-magic-user.ts
@@ -15,15 +15,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== "POST") {
-    res.status(405).send({ message: "Only POST requests allowed" });
-    return;
-  }
-
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Content-Type", "application/json");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
 
   const { email } = req.body as RequestBody;
 
