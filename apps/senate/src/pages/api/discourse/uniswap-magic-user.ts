c43488d04898b9ee57c7eaee876e33bbdb9c00ca
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-07-25 20:12:53 +0300

    fix: cors?

--- apps/senate/src/pages/api/discourse/uniswap-magic-user.ts
+++ apps/senate/src/pages/api/discourse/uniswap-magic-user.ts
@@ -20,6 +20,11 @@ export default async function handler(
     return;
   }
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Content-Type", "application/json");
+
   const { email } = req.body as RequestBody;
 
   const existingUser = await prisma.user.findFirst({
