e15a49eef574ba0146829fb4668a5540c1d80147
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-09-12 20:45:05 +0100

    ci: fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 const port = 3000;
-const baseURL = `http://localhost:${port}`;
+const baseURL = `http://127.0.0.1:${port}`;
 
 export default defineConfig({
   testDir: "./tests",
